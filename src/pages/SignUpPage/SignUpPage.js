743b8d94e8c976933ff2c7e3b715feb661588ca4
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-04-18 13:03:03 +0900

    signUpPage 생성

---
+++ src/pages/SignUpPage/SignUpPage.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import ClientTemplate from "../../templates/ClientTemplate";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+
+const SignUpContent = styled.div`
+  width: 100%;
+  height:100vh;
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f2f2f2;
+`;
+
+const SignUpCard = styled.form`
+  width: 600px;
+  height: 400px;
+  padding: 50px 20px;
+  background-color: #fff;
+`;
+
+const InsertDetail = styled.form`
+  width: 600px;
+  padding: 50px 20px;
+  background-color: #fff;
+`
+
+const FromFilde = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`
+
+class SignUpPage extends Component {
+  state = {
+    useDetail: false
+  }
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    let data = event.target
+    let formData = {
+      email: data.email.value,
+      password: data.password.value,
+      nickName: data.nick_name.value
+    }
+    console.log("form", formData);
+    this.props.SignUpRequest(formData).then(data => {
+      this.props.history.push("/design");
+    });
+  }
+  onClickFBSignUpbtn = (data) => {
+    console.log(data);
+    let formData = {
+      email: null,
+      nickName: null,
+      FBUserId: null
+    }
+    if (data.email == null) {
+      formData.email = prompt("email을 입력해 주세요.");
+    } else {
+      formData.email = data.email
+      formData.nickName = data.name
+      formData.FBUserId = data.userID
+    }
+    this.props.FBSignUpRequest(formData).then(data => {
+      console.log("fbsignup: ", data);
+      if (data.success === false) {
+        alert(data.message);
+      } else {
+        this.props.history.push("/design");
+      }
+    });
+  }
+  handleDetailFormSubmit = (data) => {
+    console.log("data");
+  }
+  render() {
+    return (
+      <ClientTemplate>
+        <SignUpContent>
+          {this.state.useDetail
+            ? <InsertDetail method="post" onSubmit={this.handleDetailFormSubmit}>
+              <FromFilde>
+                <label htmlFor="email">email</label>
+                <input type="email" id="email" name="email" />
+              </FromFilde>
+              <FromFilde>
+                <label htmlFor="password">passord</label>
+                <input type="password" id="password" name="password" />
+              </FromFilde>
+              <FromFilde>
+                <label htmlFor="nick_name">Nick Name</label>
+                <input type="text" id="nick_name" name="nick_name" />
+              </FromFilde>
+            </InsertDetail>
+            : <SignUpCard method="post" onSubmit={this.handleFormSubmit}>
+              <FromFilde>
+                <label htmlFor="email">email</label>
+                <input type="email" id="email" name="email" />
+                <button type="button">email 중복검사</button>
+              </FromFilde>
+              <FromFilde>
+                <label htmlFor="password">passord</label>
+                <input type="password" id="password" name="password" />
+              </FromFilde>
+              <FromFilde>
+                <label htmlFor="nick_name">Nick Name</label>
+                <input type="text" id="nick_name" name="nick_name" />
+              </FromFilde>
+              <button type="submit">회원가입 하기</button>
+              <FacebookLogin
+                appId="1846803492017708"
+                fields="name,email"
+                autoLoad={false}
+                callback={this.onClickFBSignUpbtn}
+                render={renderProps => (
+                  <button type="button" onClick={renderProps.onClick}>facebook 회원가입</button>
+                )} />
+            </SignUpCard>
+          }
+        </SignUpContent>
+      </ClientTemplate>
+    );
+  }
+}
+
+export default SignUpPage;
