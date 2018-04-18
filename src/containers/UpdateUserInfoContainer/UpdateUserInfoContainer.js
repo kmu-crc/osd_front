7acf418331d5f8fd60b4e7ffabbdd7878f391622
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-04-18 13:11:20 +0900

    updateUser 페이지 생성 및 router 연결... 미완성

---
+++ src/containers/UpdateUserInfoContainer/UpdateUserInfoContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UpdateUserInfo from "../../pages/UpdateUserInfo";
+
+class UpdateUserInfoContainer extends Component {
+  render() {
+    return(
+      <UpdateUserInfo />
+    );
+  }
+}
+
+export default UpdateUserInfoContainer;
