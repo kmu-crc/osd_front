c12714352f206929a3a82ffc10be6d653d5bf9b5
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-08 03:41:41 +0900

    기존에는 page가 container에 연결되어있었던걸 from component로 변경

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import ClientTemplate from "../../templates/ClientTemplate";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
-import { SetSession } from "../../modules/Sessions";
+import SignInContainer from "../../containers/SignInContainer";
 
 const SignUpContent = styled.div`
   width: 100%;
@@ -15,67 +14,19 @@ const SignUpContent = styled.div`
   background-color: #f2f2f2;
 `;
 
-const SignUpCard = styled.form`
+const SignUpCard = styled.div`
   width: 600px;
-  height: 400px;
   background-color: #fff;
+  padding: 30px;
 `;
 
 class SignInPage extends Component {
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    let data = event.target
-    let formData = {
-      email: data.email.value,
-      password: data.password.value
-    }
-    console.log("form", formData);
-    this.props.SignInRequest(formData).then( data => {
-      if(data.type === "AUTH_SIGNIN_IS_NOT_MEMBER"){
-        alert("opendesign회원이 아닙니다.");
-      } else if (data.type === "AUTH_SIGNIN_IS_NOT_PASSWORD") {
-        alert("비밀번호가 일치하지 않습니다.");
-      } else {
-        SetSession("opendesign_token", data.token).then(() => {
-          this.props.history.push("/design");
-        }).catch();
-      }
-    });
-  }
-  onClickFBSignUpbtn = (data) => {
-    console.log(data)
-    let formData = {
-      FBUserId: data.userID
-    }
-    this.props.FBSignInRequest(formData).then( data => {
-      if(data.type === "AUTH_FBSIGNIN_IS_NOT_MEMBER") {
-        alert("opendesign회원이 아닙니다.");
-      } else {
-        SetSession("opendesign_token", data.token).then(() => {
-          this.props.history.push("/design");
-        }).catch();
-      }
-    });
-  }
   render() {
     return (
       <ClientTemplate>
         <SignUpContent>
-          <SignUpCard method="post" onSubmit={this.handleFormSubmit}>
-            <label htmlFor="email">email</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="password">passord</label>
-            <input type="password" id="password" name="password" />
-            <button type="submit">로그인 하기</button>
-
-            <FacebookLogin
-              appId="1846803492017708"
-              fields="name,email"
-              autoLoad={false}
-              callback={this.onClickFBSignUpbtn}
-              render={renderProps => (
-                <button type="button" onClick={renderProps.onClick}>facebook 로그인</button>
-              )} />
+          <SignUpCard>
+            <SignInContainer />
           </SignUpCard>
         </SignUpContent>
       </ClientTemplate>
