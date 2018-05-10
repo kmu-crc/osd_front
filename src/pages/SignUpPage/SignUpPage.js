d0e4bb1eed4371b953d6a263db3e53e0c486d696
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-11 01:50:11 +0900

    필요없는 로직 삭제

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -14,7 +14,7 @@ const SignUpContent = styled.div`
   background-color: #f2f2f2;
 `;
 
-const SignUpCard = styled.form`
+const SignUpCard = styled.div`
   width: 600px;
   padding: 50px 20px;
   background-color: #fff;
@@ -32,66 +32,12 @@ const FromFilde = styled.div`
 `
 
 class SignUpPage extends Component {
-  state = {
-    useDetail: false,
-    checkEmail: false
-  }
-  handleFormSubmit = (data) => {
-    console.log(data);
-    let formData = {
-      email: data.email,
-      password: data.password,
-      nick_name: data.nick_name
-    }
-    // this.props.SignUpRequest(formData).then(data => {
-    //   this.props.history.push("/design");
-    // });
-  }
-  onClickFBSignUpbtn = (data) => {
-    console.log(data);
-    let formData = {
-      email: null,
-      nickName: null,
-      FBUserId: null
-    }
-    if (data.email == null) {
-      formData.email = prompt("email을 입력해 주세요.");
-    } else {
-      formData.email = data.email
-      formData.nickName = data.name
-      formData.FBUserId = data.userID
-    }
-    this.props.FBSignUpRequest(formData).then(data => {
-      console.log("fbsignup: ", data);
-      if (data.success === false) {
-        alert(data.message);
-      } else {
-        this.props.history.push("/design");
-      }
-    });
-  }
-  handleDetailFormSubmit = (data) => {
-    console.log("data");
-  }
-
-  handleEmailCheck = () => {
-    this.props.CheckEmailRequest(this.email.value).then(data => {
-      console.log(data);
-      if (data.checkEmail) {
-        alert("사용가능한 email입니다.");
-        this.setState({ checkEmail: true });
-      } else {
-        alert(data.error);
-        this.setState({ checkEmail: false });
-      }
-    });
-  }
   render() {
     return (
       <ClientTemplate>
         <SignUpContent>
           <InsertDetail>
-            <SignUpContainer method="post" onSubmit={this.handleFormSubmit} />
+            <SignUpContainer onSubmit={this.handleFormSubmit} />
           </InsertDetail>
         </SignUpContent>
       </ClientTemplate>
