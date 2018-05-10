a89106ad9ba8988ebb9ebf4d85680a6966f56061
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-11 01:46:30 +0900

    특수문자 사용제한 검증 로직 추가, facebook 가입여부 검증 로직 추가

--- src/modules/Validates/Validates.js
+++ src/modules/Validates/Validates.js
@@ -2,7 +2,7 @@ const Validates = {};
 
 Validates.required = value => {
   return new Promise((resolve, reject) => {
-    let data = value ? undefined : 'Required';
+    let data = value ? undefined : "Required";
     resolve(data);
   });
 };
@@ -11,7 +11,17 @@ Validates.email = value => {
   return new Promise((resolve, reject) => {
     if(!value) resolve(undefined);
     let data = value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email address'
+    ? "잘못된 E-Mail형식입니다."
+    : undefined
+    resolve(data);
+  });
+};
+
+Validates.NotSpecialCharacters = value => {
+  return new Promise((resolve, reject) => {
+    if(!value) resolve(undefined);
+    let data = value && !/^[a-zA-Zㄱ-힣0-9]*$/i.test(value)
+    ? "특수문자는 사용할 수 없습니다."
     : undefined
     resolve(data);
   });
@@ -19,7 +29,7 @@ Validates.email = value => {
 
 
 Validates.checkEmail = (value) => {
-  return fetch("http://localhost:8080/users/checkEmail", { headers: { 'Content-Type': 'application/json' }, method: "POST", body: JSON.stringify({ email: value }) })
+  return fetch("http://localhost:8080/users/checkEmail", { headers: { "Content-Type": "application/json" }, method: "POST", body: JSON.stringify({ email: value }) })
         .then(res => res.json())
         .then(res => {
           if(!value) return Promise.resolve(undefined);
@@ -30,7 +40,18 @@ Validates.checkEmail = (value) => {
 };
 
 Validates.checkNickName = (value) => {
-  return fetch("http://localhost:8080/users/checkNickName", { headers: { 'Content-Type': 'application/json' }, method: "POST", body: JSON.stringify({ nick_name: value }) })
+  return fetch("http://localhost:8080/users/checkNickName", { headers: { "Content-Type": "application/json" }, method: "POST", body: JSON.stringify({ nick_name: value }) })
+        .then(res => res.json())
+        .then(res => {
+          if(!value) return Promise.resolve(undefined);
+          return res.success
+          ? undefined
+          : res.error
+        })
+};
+
+Validates.checkFBUser = (value) => {
+  return fetch("http://localhost:8080/users/checkFBUser", { headers: { "Content-Type": "application/json" }, method: "POST", body: JSON.stringify({ FB_user_id: value }) })
         .then(res => res.json())
         .then(res => {
           if(!value) return Promise.resolve(undefined);
