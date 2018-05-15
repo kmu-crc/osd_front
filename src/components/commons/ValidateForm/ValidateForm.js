ca13e8236d20fdb7c243c814c98bc33b87f22fbb
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-16 01:43:10 +0900

    submit하면 formData를 생성하게 로직 변경

--- src/components/commons/ValidateForm/ValidateForm.js
+++ src/components/commons/ValidateForm/ValidateForm.js
@@ -7,30 +7,22 @@ class ValidateForm extends Component {
   handelSubmit = (event) => {
     event.preventDefault();
     const target = event.target;
-    let fromData = {};
+    console.log(target);
+    const formData = new FormData(target);
     for (let data of Array.from(target.elements)) {
       if (data.name) {
         if (!data.attributes.status) {
-          fromData = {};
           this.setState({ submit: false });
           data.focus();
           break;
         } else {
-          if(data.type === "file"){
-            fromData[data.name] = data.files[0];
-          } else if(data.type === "checkbox") {
-            fromData[data.name] = data.checked;
-          } else {
-            fromData[data.name] = data.value;
-          }
-
           this.setState({ submit: true });
         }
       }
     }
     setTimeout(() => {
       if (this.state.submit) {
-        this.props.onSubmit(fromData);
+        this.props.onSubmit(formData);
       }
     }, 100);
   }
