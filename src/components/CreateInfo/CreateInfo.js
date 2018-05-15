4ed7d0b4567f6d3e1caf974b94485c0410d1d1f5
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-16 01:44:31 +0900

    새로만든 Form요소로 createInfo 수정

--- src/components/CreateInfo/CreateInfo.js
+++ src/components/CreateInfo/CreateInfo.js
@@ -1,136 +1,59 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Header, Grid, Form } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { InputField, TextAreaField, SelectField, FileField, CheckBoxField } from "../commons/FormField";
 
-
-// css styling
-
-const FormContainer = styled.div`
-  width: 80%;
-  margin: auto;
-  background-color: #fff;
-  & .mainInfo, & .authInfo {
-    padding: 20px 30px 20px 40px;
-    box-shadow: 0 1px 5px #c7c7c7;
-  }
-  & .authInfo {
-    margin-top: 10px;
-  }
-`;
-
-const List = styled.div`
+const FromFieldCard = styled.div`
   width: 100%;
-  height: 60px;
-  & label {
-    display: block;
-    width: 20%;
-    line-height: 60px;
-    text-align: left;
-    float: left;
-    min-width: 88px;
-  }
-  & select {
-    width: 20%;
-    height: 30px;
-    margin-top: 15px;
-    margin-right: 10px;
-  }
-`;
-
-const Input = styled.input`
-  width: 75%;
-  height: 30px;
-  margin-top: 10px;
-  float: left;
-`;
-
-const FileInput = styled.input`
-  width: 50%;
-  float: left;
-  height: 30px;
-  margin-top: 10px;
-`;
-
-const AuthCheck = styled.div`
-  float: left;
-  & label {
-    display: inline;
-    width: 80px;
-    text-align: right;
-    font-size: 15px;
-  }
-  & input {
-    width: 20px;
-    float: left;
-    margin-right: 40px;
-    margin-top: 22px;
-  }
-`;
+  background-color: white;
+  box-shadow: 3px 3px 3px rgba(0,0,0,0.3);
+  padding: 40px;
+  margin-bottom: 30px;
+`
 
 class CreateInfo extends Component{
   render(){
     return(
-        <FormContainer>
-          <form>
-            <div className="mainInfo">
-              <h3>디자인 정보 입력</h3>
-              <List>
-                <label>디자인 제목</label>
-                <Input placeholder="디자인 제목을 입력해 주세요.(최대 20자)"/>
-              </List>
-              <List>
-                <label>디자인 설명</label>
-                <Input placeholder="내용을 입력해 주세요."/>
-              </List>
-              <List>
-                <label>썸네일 업로드</label>
-                <FileInput type="file" />
-              </List>
-              <List>
-                <label>카테고리</label>
-                <select>
-                  <option>의상</option>
-                  <option>자동차</option>
-                  <option>웹</option>
-                </select>
-                <select>
-                  <option>의상</option>
-                  <option>자동차</option>
-                  <option>웹</option>
-                </select>
-              </List>
-              <List>
-                <label>멤버추가</label>
-                <select>
-                  <option></option>
-                </select>
-              </List>
-            </div>
-            <div className="authInfo">
-              <h3>권한 설정</h3>
-                <List>
-                  <label>라이센스</label>
-                  <AuthCheck>
-                    <label>수정 가능</label>
-                    <input type="checkbox"/>
-                    <label>상업적 이용</label>
-                    <input type="checkbox"/>
-                    <label>원작자 표시</label>
-                    <input type="checkbox"/>
-                  </AuthCheck>
-                </List>
-                <div className="clear"></div>
-                <List>
-                  <label>디자인 공개 여부</label>
-                  <AuthCheck>
-                    <label>공개</label>
-                    <input type="checkbox"/>
-                  </AuthCheck>
-                  <div className="clear"></div>
-                </List>
-              </div>
-          </form>
-      </FormContainer>
+      <div>
+<FromFieldCard>
+      <Grid divided={true}>
+        <Grid.Column width={3}>
+          <Header as="h2">디자인 정보</Header>
+        </Grid.Column>
+        <Grid.Column width={13}>
+          <Form.Group widths="equal">
+            <InputField name="title" label="디자인 제목" placeholder="디자인의 제목을 입력해주세요." type="text" validates={["required"]} />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <TextAreaField type="text" name="explanation" placeholder="디자인 설명을 입력해주세요." label="디자인 설명" />
+          </Form.Group>
+          <Form.Group  widths="equal">
+            <FileField name="thumbnail" placeholder="섬네일 이미지를 등록하세요." label="섬네일"/>
+          </Form.Group>
+          <Form.Group widths={2}>
+            <SelectField name="category_level1" options={[{ text: "디자인", value: 1 }, { text: "스마트", value: 2 }]} label="카테고리" placeholder="카테고리를 골라주세요." validates={["required"]} />
+            <SelectField name="category_level2" options={[{ text: "시각", value: 1 }, { text: "패션", value: 2 }]} label="카테고리2" placeholder="카테고리2를 골라주세요." validates={["required"]} />
+          </Form.Group>
+        </Grid.Column>
+      </Grid>
+    </FromFieldCard>
+    <FromFieldCard>
+    <Grid divided={true}>
+    <Grid.Column width={3}>
+          <Header as="h2">라이센스</Header>
+        </Grid.Column>
+        <Grid.Column width={13}>
+        <Form.Group widths={4}>
+            <CheckBoxField name="is_copy" label="상업적 이용" placeholder="허가" />
+            <CheckBoxField name="is_display_creater" label="원작자 표시" placeholder="필수" />
+            <CheckBoxField name="is_modify" label="수정 가능" placeholder="허가" />
+            <CheckBoxField name="is_share" label="디자인 공개 여부" placeholder="공개" />
+          </Form.Group>
+        </Grid.Column>
+    </Grid>
+    </FromFieldCard>
+      </div>
     );
   }
 }
