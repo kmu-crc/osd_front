0190167be7f01f4d831c2e39ef694513363430c6
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-16 01:45:02 +0900

    CreateDesign 로직 수정중...

--- src/components/CreateDesign/CreateDesign.js
+++ src/components/CreateDesign/CreateDesign.js
@@ -1,61 +1,18 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import CreateInfo from "../CreateInfo";
 import CreateView from "../CreateView";
-import { Container, Row } from "../Grid";
 import { Link } from "react-router-dom";
-import { Grid } from "semantic-ui-react";
+import { Container, Header, Grid, Form, Button } from "semantic-ui-react";
+import ValidateForm from "../commons/ValidateForm";
+import CreateInfo from "../CreateInfo";
 
-// css styling
-
-const Wrapper = styled.div`
-  background-color: #f9f9f9;
+const FromFieldCard = styled.div`
   width: 100%;
-  min-width: 660px;
-  padding: 20px 30px 30px;
-  position: relative;
-  & .ui.grid {
-    margin: 0;
-  }
-`;
-
-const Header = styled.h2`
-  font-weight: 400;
-  padding-left: 50px;
-  color: #D7382C;
-`;
-
-const RouterContainer = Container.extend`
-`;
-
-const CheckTemp = styled.div`
-  width: 80%;
-  margin: 20px auto;
-  & span {
-    float: left;
-    font-size: 16px;
-  }
-  & input {
-    float: left;
-    width: 20px;
-    height: 20px;
-    margin-right: 20px;
-  }
-`;
-
-const MainBtn = styled.button`
-  padding: 7px 25px;
-  border-radius: 30px;
-  background-color: #EB3324;
-  border: none;
-  color: #fff;
-  font-size: 14px;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  &:hover {
-    background-color: #CD4533;
-  }
-`;
+  background-color: white;
+  box-shadow: 3px 3px 3px rgba(0,0,0,0.3);
+  padding: 40px;
+  margin-bottom: 30px;
+`
 
 class CreateDesign extends Component {
   state = {
@@ -72,28 +29,15 @@ class CreateDesign extends Component {
     console.log("file upload");
   }
 
-  render(){
-    return(
-      <Wrapper>
-        <Header>새 디자인 등록</Header>
-        <RouterContainer>
-          <CreateInfo/>
-          <CheckTemp>
-            <span>디자인형</span>
-            <input type="checkbox" checked={this.state.useProject === false && "checked"} onChange={this.onProjectActive}/>
-            <span>프로젝트형</span>
-            <input type="checkbox" checked={this.state.useProject === true && "checked"} onChange={this.onProjectActive}/>
-            <Row/>
-          </CheckTemp>
-          {this.state.useProject === false &&
-            <CreateView fileTrue={this.goFileTrue}/>
-          }
-        </RouterContainer>
-        <Row/>
-        <Grid container={true} textAlign="center">
-          <Link to="/design"><MainBtn>등록</MainBtn></Link>
-        </Grid>
-      </Wrapper>
+  render() {
+    return (
+      <Container>
+        <Header as='h1'>디자인 등록</Header>
+        <ValidateForm onSubmit={this.goFileTrue}>
+          <CreateInfo />
+          <Button type="submit">등록</Button>
+        </ValidateForm>
+      </Container>
     );
   }
 }
