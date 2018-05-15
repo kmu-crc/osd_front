124f2b28a4c3ef42b873b0ca669a18070f1006de
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-16 01:41:50 +0900

    getCategory props 추가

--- src/containers/UserDetailFormContainer/UserDetailFormContainer.js
+++ src/containers/UserDetailFormContainer/UserDetailFormContainer.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import UserDetailForm from "../../components/UserDetailFrom";
 import { InsertUserDetailRequest } from "../../actions/Users";
+import { GetCategoryLevel1Request, GetCategoryLevel2Request } from "../../actions/Categorys";
+
 class UpdateUserInfoContainer extends Component {
   render() {
     return (
@@ -13,7 +15,9 @@ class UpdateUserInfoContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    token: state.Authentication.status.token
+    token: state.Authentication.status.token,
+    category1: state.Categorys.status.level1,
+    category2: state.Categorys.status.level2
   };
 };
 
@@ -21,6 +25,12 @@ const mapDispatchToProps = (dispatch) => {
   return {
     InsertUserDetailRequest: (data, token) => {
       return dispatch(InsertUserDetailRequest(data, token));
+    },
+    GetCategoryLevel1Request: () => {
+      return dispatch(GetCategoryLevel1Request());
+    },
+    GetCategoryLevel2Request: (id) => {
+      return dispatch(GetCategoryLevel2Request(id));
     }
   };
 };
