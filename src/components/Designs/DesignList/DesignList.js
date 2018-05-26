6d635de1f87bced650d45d232ff845ae5bce7c39
seoyeonyeom <sy8095@gmail.com>
2018-05-26 14:02:22 +0900

    페이지 로딩 스크롤 이벤트

--- src/components/Designs/DesignList/DesignList.js
+++ src/components/Designs/DesignList/DesignList.js
@@ -4,6 +4,7 @@ import { Grid, Loader, Dimmer } from "semantic-ui-react";
 import Category from "components/Commons/Category";
 import Sorting from "components/Commons/Sorting";
 import ContentList from "components/Commons/ContentList";
+import InfiniteScroll from 'react-infinite-scroller';
 
 // css styling
 
@@ -40,29 +41,24 @@ const MenuContainer = styled(Grid)`
   }
 `;
 
-
 class DesignList extends Component {
   state = {
-    page: 0,
-    loading: false
-  }
-
-  componentWillMount(){
-    this.props.GetDesignListRequest(this.state.page, this.props.sort, this.props.cate1, this.props.cate2);
-  }
+    hasMore: true,
+    loading: false,
+  };
 
   shouldComponentUpdate(nextProps){
     if (JSON.stringify(nextProps.DesignList) === JSON.stringify(this.props.DesignList)) {
       return false;
     } else {
+      console.log("et");
       return true;
     }
-    
   }
 
   sortChange = (e, {value}) => {
     this.props.history.replace(`/design/${value}/${this.props.cate1}/${this.props.cate2}`);
-    this.props.GetDesignListRequest(0, value, this.props.cate1, this.props.cate2);
+    //this.props.GetDesignListRequest(0, value, this.props.cate1, this.props.cate2);
   }
 
   cate1Change = (e, {value}) => {
@@ -75,58 +71,37 @@ class DesignList extends Component {
     this.props.GetDesignListRequest(0, this.props.sort, this.props.cate1, value);
   }
 
-  touchBottom = (e) => {
-    // console.log(document.body.scrollHeight) // 문서 전체의 길이
-    // console.log(document.body.offsetHeight) // 보이는 창의 길이
-    // console.log(window.scrollY); // 스크롤의 시작 높이 (위에 가려진 컨텐츠 길이와 같음)
-    // if (window.scrollY + document.body.offsetHeight === document.body.scrollHeight) {
-    //   console.log("bottom");
-    //   this.setState({
-    //     loading: true
-    //   });
-    //   if (this.state.loading === false) {
-    //     this.getLoadData();
-    //   } else {
-    //     return;
-    //   }
-    // }
-  }
-
-  getLoadData = () => {
-    console.log("함수가 호출되는 횟수");
-    this.props.GetDesignListRequest(this.state.page+1, this.props.sort, this.props.cate1, this.props.cate2)
-    .then(() => {
-      this.setState({
-        page: this.state.page + 1
+  getLoadData = (page) => {
+      this.props.GetDesignListRequest(page, this.props.sort, this.props.cate1, this.props.cate2)
+      .then(() => {
+        this.setState({
+          hasMore: this.props.DesignList.length === 0? false : true
+        });
       });
-    })
-    .then(() => {
-      this.setState({
-        loading: false
-      });
-    });
   }
 
   render(){
-    let list = this.props.DesignList;
+    console.log("렌더링됨");
+    let listAll = this.props.DesignListAdded;
+    console.log(listAll);
+    let item = [];
+    listAll.length !== 0 && listAll.map((list, i)=> 
+      item.push(<ContentList key={i} data={list} user={this.props.userInfo} type="design" columns={5}/>)
+    );
+
     return(
-      <Wrapper onWheel={this.touchBottom}>
+      <Wrapper>
         <MenuContainer devided="vertically" padded={true} columns={2}>
           <Grid.Row stretched={false}>
             <Category computer={8} tablet={10} mobile={12} handleCate1={this.cate1Change} handleCate2={this.cate2Change}/>
             <Sorting computer={8} tablet={6} mobile={4} handleChange={this.sortChange}/>
           </Grid.Row>
         </MenuContainer>
-        {this.state.page === 0?
-        <ContentList data={list} user={this.props.userInfo} type="design" columns={5}/>
-        :
-        this.props.DesignListAdded.map(list =>
-          <ContentList key={this.state.page} data={list} user={this.props.userInfo} type="design" columns={5}/>
-        )
-        }
-        <Dimmer active={this.state.loading? true : false}>
-          <Loader active={this.state.loading? true : false} inline="centered" size="huge"/>
-        </Dimmer>
+        <InfiniteScroll threshold={700} pageStart={-1} loadMore={this.getLoadData} 
+                        hasMore={this.state.hasMore}
+                        loader={ <Loader active={this.state.loading? true : false} inline="centered" size="huge" key={0}/> }>
+          {item}
+        </InfiniteScroll>
       </Wrapper>
     );
   }
