ecdbd4a6d7df551ef95c1345864cd86e0eae8003
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-05-30 17:40:12 +0900

    ㅅㅓㄹ정 파일 변경

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 let host = null;
-if(process.env.REACT_APP_BUILD) {
-  host = "ec2-13-124-212-254.ap-northeast-2.compute.amazonaws.com";
+const build = false;
+if(build) {
+  host = "http://ec2-13-124-212-254.ap-northeast-2.compute.amazonaws.com";
 } else {
   host = "http://localhost:8080";
 }
