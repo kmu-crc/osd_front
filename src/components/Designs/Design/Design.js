b0ccb710512f7edaacc6b1ce3f58aa167cac31fc
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-05-31 18:35:42 +0900

    design Card 형태 변경

--- src/components/Designs/Design/Design.js
+++ src/components/Designs/Design/Design.js
@@ -8,18 +8,15 @@ import { Icon } from "semantic-ui-react";
 
 const Designli = styled.li`
   width: 100%;
-  margin: 0 auto 40px;
-  height: 270px;
+  margin: 0 auto 2rem;
   font-size: 13px;
-  border-radius: 6px 6px 3px 3px;
+  border-radius: 3px 3px 3px 3px;
+  overflow: hidden;
   box-shadow: 0 1px 2px rgba(25,25,25,0.2);
   background-color: #fff;
   & a {
     cursor: pointer;
     display: block;
-  }
-  &:hover {
-    border-bottom: 0.5px solid dimgray;
   }
 `;
 
@@ -34,63 +31,69 @@ const ImgPart = styled.div`
 `;
 
 const TextPart = styled.div`
+  padding: 10px 10px;
   & .title, & .userName {
-    height: 36px;
     line-height: 1.35;
-    padding: 10px 0;
-    margin: 0 10px;
   }
   & .title {
     font-weight: bold;
+    line-height: 20px;
+    height: 40px;
+    margin: 10px 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
   }
   & .userName {
-    border-bottom: 1px solid #f2f2f2;
   }
   & .cate {
-    color: dimgray;
-    padding: 10px 0 5px 10px;
+    color: #EB3324;
     font-weight: 300;
+    font-size: 12px;
   }
 `;
 
 const Count = styled.div`
-  color: dimgray;
+  background-color: #000;
+  padding: 5px 10px;
+  color: white;
   font-weight: 400;
   font-size: 12px;
   & div {
-    float: right;
+    float: left;
     width: 40px;
   }
-  & div.clear {
+  &::after{
+    display: block;
+    content: "";
     clear: both;
-    float: none;
   }
 `;
 
 class Design extends Component {
-  render(){
+  render() {
     let design = this.props.design;
     let user = this.props.user;
-    return(
-      <NavLink to={"/designDetail/"+design.uid}>
+    return (
+      <NavLink to={"/designDetail/" + design.uid}>
         <Designli>
-          <ImgPart><img src={eximg} alt="썸네일이미지"/></ImgPart>
+          <ImgPart><img src={eximg} alt="썸네일이미지" /></ImgPart>
           <TextPart>
+            <div className="cate">{design.categoryName}</div>
             <div className="title">{design.title}</div>
             <div className="userName">{design.userName}</div>
-            <div className="cate">{design.categoryName}</div>
-            <Count>
-              <div>
-                <Icon name="unhide" color="grey" size="mini"></Icon>
-                {design.total_view_count? design.total_view_count : 0}
-              </div>
-              <div>
-                <Icon name="heart" color="grey" size="mini"></Icon>
-                {design.like_count? design.like_count : 0}
-              </div>
-              <div className="clear"></div>
-            </Count>
           </TextPart>
+          <Count>
+            <div>
+              <Icon name="unhide" color="white" size="mini"></Icon>
+              {design.total_view_count ? design.total_view_count : 0}
+            </div>
+            <div>
+              <Icon name="heart" color="white" size="mini"></Icon>
+              {design.like_count ? design.like_count : 0}
+            </div>
+          </Count>
         </Designli>
       </NavLink>
     );
