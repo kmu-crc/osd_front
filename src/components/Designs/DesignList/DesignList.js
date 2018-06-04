7a7d0a082bf73f18e7468a5f7179f5af14bdbf46
seoyeonyeom <sy8095@gmail.com>
2018-06-04 15:25:00 +0900

    디자인 리셋 리퀘스트 함수 추가

--- src/components/Designs/DesignList/DesignList.js
+++ src/components/Designs/DesignList/DesignList.js
@@ -10,7 +10,6 @@ import ScrollDesignListContainer from "containers/Designs/ScrollDesignListContai
 const Wrapper = styled.div`
   background-color: #f9f9f9;
   width: 100%;
-  padding-top: 8rem;
   padding-bottom: 5rem;
   min-width: 660px;
   & ul {
