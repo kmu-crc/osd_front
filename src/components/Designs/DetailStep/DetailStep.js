73c4d9df3566e1d5c880c6a3389204c7dfcc8f16
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-04 17:04:07 +0900

    setp 로직 수정중

--- src/components/Designs/DetailStep/DetailStep.js
+++ src/components/Designs/DetailStep/DetailStep.js
@@ -6,7 +6,7 @@ import { Grid } from "semantic-ui-react";
 
 // css styling
 
-const Container = styled(Grid)`
+const Container = styled(Grid) `
   min-width: 660px;
   & .ul {
     width: 100%;
@@ -56,60 +56,83 @@ class DetailStep extends Component {
   };
 
   activeEdit = (e) => {
+    const target = e.target;
     this.setState({
-      isEdit: e.target.id,
-      titleValue: e.target.innerHTML
+      isEdit: target.id,
+      titleValue: target.textContent
     });
+    setTimeout(() => {
+      this[`input${target.id}`].focus();
+    },10)
   }
 
   onEdit = (e) => {
+    const target = e.target;
+    const value = target.value;
     this.setState({
-      titleValue: e.target.value
+      titleValue: value
     });
   }
+  endEdit = (target) => {
+    this.setState({
+      isEdit: -1,
+    });
+  }
+  endBlurEdit = (e) => {
+    const target = e.target;
+    this.endEdit(target);
+  }
+  endKeyEdit = (e) => {
+    const target = e.target;
+    const key = e.keyCode;
+    console.log(key);
+    if(key === 13){
+      this.endEdit(target);
+    }
+  }
 
-  render(){
+  render() {
     let step = this.props.DesignDetailStep;
-    return(
+    return (
       <Container>
         <ul>
           {step.length !== 0 && step.map((board, i) =>
             <Board key={i}>
               <div className="boardList">
-                {this.state.isEdit == board.uid? 
-                  <input className="boardTitle" value={this.state.titleValue} onChange={this.onEdit}/>
+                {this.state.isEdit == board.uid ?
+                  <input className="boardTitle" ref={ref => this[`input${board.uid}`] = ref} value={this.state.titleValue} onKeyDown={this.endKeyEdit} onChange={this.onEdit} />
                   : <h4 id={board.uid} onClick={this.activeEdit}>
-                      {board.title}
-                    </h4>
+                    {board.title}
+                  </h4>
                 }
                 <div>
                   <SortablePane direction="vertical" margin={5}>
-                  {board.cardData.length > 0 && board.cardData.map(card =>
-                    board.cardData.length > 1? 
-                    <Pane key={card.uid} id={card.uid}
+                    {board.cardData.length > 0 && board.cardData.map(card =>
+                      board.cardData.length > 1 ?
+                        <Pane key={card.uid} id={card.uid}
                           className="pane" width="100%" height={80} maxHeight={120}
                           onResizable={{ x: false, y: false, xy: false }}>
-                      <DesignStepCard cardDetail={card} designId={board.design_id}/>
-                    </Pane>
-                    :
-                    <DesignStepCard key={card.uid} cardDetail={card} designId={board.design_id}/>
-                  )}
+                          <DesignStepCard cardDetail={card} designId={board.design_id} />
+                        </Pane>
+                        :
+                        <DesignStepCard key={card.uid} cardDetail={card} designId={board.design_id} />
+                    )}
                   </SortablePane>
                 </div>
               </div>
             </Board>
-            )}
-            <Board>
-              <div className="boardList">
-                {this.state.isEdit === "new"? 
-                  <input value={this.state.titleValue} onChange={this.onEdit}/>
-                  : <h4 id="new" onClick={this.activeEdit}>
-                      새 주제 추가 +
+          )}
+          <Board>
+            <div className="boardList">
+              {this.state.isEdit === "0" ?
+                <input placeholder="새 주제 추가" ref={ref => this["input0"] = ref} onChange={this.onEdit} onKeyDown={this.endKeyEdit} onBlur={this.endBlurEdit}/>
+                : <h4 id="0" onClick={this.activeEdit}>
+                  새 주제 추가
                     </h4>
-                }
-                <EmptyCard>새 카드 추가 +</EmptyCard>
-              </div>
-            </Board>
+              }
+              <EmptyCard>새 카드 추가 +</EmptyCard>
+            </div>
+          </Board>
         </ul>
       </Container>
     );
