f135cf397bc75c236bbb7656f6a783130ed90a4c
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-04 17:02:15 +0900

    template에서 contentBox 제거

--- src/templates/ClientTemplate/ClientTemplate.js
+++ src/templates/ClientTemplate/ClientTemplate.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import HeaderContainer from "containers/Commons/HeaderContainer";
 import Footer from "components/Commons/Footer";
-import ContentBox from "components/Commons/ContentBox";
 
 class ClientTemplate extends Component {
   render() {
-    return(
-      <div style={{position: "relative"}}>
+    return (
+      <div style={{ position: "relative" }}>
         <HeaderContainer />
-        <ContentBox>
-          {this.props.children}
-        </ContentBox>
+        {this.props.children}
         <Footer />
       </div>
     );
