1d6e24aaed8bd883cbdc761dededf82345fbe37c
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-08 20:52:29 +0900

    card component 생성

---
+++ src/components/Designs/DesignBoardCard/DesignBoardCard.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Modal, Button } from "semantic-ui-react";
+import ValidateForm from "components/Commons/ValidateForm";
+import { FormField } from "components/Commons/FormField";
+import { FormInput, FormTextArea } from "components/Commons/FormItem";
+import FileUploader from "components/Commons/FileUploader";
+import { CardTitleUpdate, CardContentUpdate, CardImageUpdate, CardSourcUpdate } from "components/Designs/DesignBoardCard";
+
+const BoardCard = styled.li`
+  padding: 10px;
+  background-color: white;
+  border-radius: 3px;
+  margin-bottom: 5px;
+  cursor: pointer;
+`
+
+const CustomModal = styled(Modal) `
+  padding: 30px;
+`
+
+class DesignBoardCard extends Component {
+  state = {
+    open: false,
+    active: "INIT"
+  }
+  componentDidMount() {
+    console.log(this.props.card);
+  }
+  onClose = () => {
+    this.setState({ open: false, active: "INIT" });
+  }
+  changeActive = (value) => {
+    setTimeout(() => {
+      this.props.GetCardDetailRequest(this.props.card.uid).then(
+        this.setState({ active: value })
+      )
+    }, 100);
+
+  }
+  openModalHandler = () => {
+    this.props.GetCardDetailRequest(this.props.card.uid).then(
+      this.setState({ open: true })
+    )
+  }
+  handleSubmit = (data) => {
+    console.log(data);
+  }
+  render() {
+    const { card, detail } = this.props;
+    const { open } = this.state;
+    console.log("detail", detail);
+    return (
+      <div>
+        <BoardCard onClick={this.openModalHandler}>
+          {card.title}
+        </BoardCard>
+        <CustomModal
+          open={open}
+          closeOnEscape={false}
+          closeOnRootNodeClick={false}
+          onClose={this.close}>
+          <Modal.Content>
+            <CardTitleUpdate uid={detail.uid} title={detail.title} active={this.state.active} changeActive={this.changeActive} token={this.props.token} request={this.props.UpdateCardTitleRequest} />
+            <CardContentUpdate uid={detail.uid} content={detail.content} active={this.state.active} changeActive={this.changeActive} token={this.props.token} request={this.props.UpdateCardContentRequest} />
+            <CardImageUpdate uid={detail.uid} token={this.props.token} images={detail.images} request={this.props.UpdateCardImagesRequest} active={this.state.active} changeActive={this.changeActive} />
+            <CardSourcUpdate uid={detail.uid} token={this.props.token} sourcesLink={detail.sources} request={this.props.UpdateCardSourcesRequest} active={this.state.active} changeActive={this.changeActive} />
+            <Button type="button" onClick={this.onClose}>Close</Button>
+          </Modal.Content>
+        </CustomModal>
+      </div >
+    );
+  }
+}
+
+export default DesignBoardCard;
