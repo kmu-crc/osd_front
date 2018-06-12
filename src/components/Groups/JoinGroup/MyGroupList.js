5c0849ddc14fc68c5fe8c6052075f6605a353854
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-12 18:05:45 +0900

    group join group my list component 생성

---
+++ src/components/Groups/JoinGroup/MyGroupList.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { FormField } from "components/Commons/FormField";
+import { FormSelect } from "components/Commons/FormItem";
+import ValidateForm from "components/Commons/ValidateForm";
+import { Button } from "semantic-ui-react";
+import FormDataToJson from "modules/FormDataToJson";
+
+class MyGroupList extends Component {
+  componentWillMount(){
+    console.log(this.props);
+    this.props.GetMyGroupListRequest(this.props.token, this.props.match.params.id).then(data => {
+      console.log(data);
+    })
+  }
+  handleSubmit = (data) => {
+    let formData = FormDataToJson(data);
+    this.props.GroupJoinGroupRequest(formData, this.props.token, this.props.match.params.id)
+    .then(data => {
+      this.props.handleCloseModal();
+    })
+  }
+  render() {
+    return(
+      <ValidateForm onSubmit={this.handleSubmit}>
+        <FormField name="join_group" label="내 그룹 리스트" options={this.props.groupList} RenderComponent={FormSelect} getValue={this.getValue}/>
+        <Button type="submit">등록</Button>
+        <Button type="button" onClick={this.props.handleCloseModal}>닫기</Button>
+      </ValidateForm>
+    );
+  }
+}
+
+export default MyGroupList;
