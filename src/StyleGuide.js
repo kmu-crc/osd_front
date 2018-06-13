0221d6b87cd756faa1648f61a615b24dd6354b15
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-14 04:26:53 +0900

    StyleGuide 설정 파일 생성

---
+++ src/StyleGuide.js
@@ -0,0 +1,69 @@
+const StyleGuide = {
+  font: {
+    notoSans: "'Noto Sans', sans-serif",
+    notoSerif: "'Noto Serif', serif",
+    lineHeight: 1.4,
+    size: {
+      heading1: "45px",
+      heading2: "36px",
+      heading3: "26px",
+      heading4: "18px",
+      paragraph: "14px",
+      small: "12px"
+    }
+  },
+  color: {
+    main: {
+      light: "#FACBCB",
+      basic: "#E72327",
+      dark: "#BF1D1F"
+    },
+    gey: {
+      light: "#EAEEEF",
+      basic: "#CED3D6",
+      dark: "#4D5256"
+    },
+    sub: {
+      bule: {
+        light: "#87B2CC",
+        basic: "#10669A",
+        dark: "#0B4B73"
+      },
+      purple: {
+        light: "#A49EB6",
+        basic: "#4A3E6D",
+        dark: "#2B2440"
+      }
+    },
+    geyScale: {
+      scale0: "#F8FAFB",
+      scale1: "#F1F5F5",
+      scale2: "#EAEEEF",
+      scale3: "#E1E4E6",
+      scale4: "#CED3D6",
+      scale5: "#A9AFB3",
+      scale6: "#878D91",
+      scale7: "#4D5256",
+      scale8: "#363A3C",
+      scale9: "#292A2B"
+    }
+  },
+  margin: {
+    basic: "1rem",
+    bottom:{
+      setcion: "3.5rem",
+      paragraph: "2rem",
+      title: "3rem",
+      small: "0.5rem"
+    }
+  },
+  gridContent: {
+    mobile: 320,
+    tablet: "700px",
+    computer: "850px",
+    largeScreen: "1100px",
+    wideScreen: "1800px",
+  }
+};
+
+export default StyleGuide;
