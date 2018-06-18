fc1bd13960a769493ba0c8f58db4ea8827728e21
seoyeonyeom <sy8095@gmail.com>
2018-06-18 20:06:39 +0900

    box-shadow, border 추가수정

--- src/components/Groups/CreateGroup/CreateGroup.js
+++ src/components/Groups/CreateGroup/CreateGroup.js
@@ -56,23 +56,43 @@ const Wrapper = styled(ContentBox)`
 const FromFieldCard = styled.div`
   width: 100%;
   background-color: white;
-  box-shadow: 3px 3px 3px rgba(0,0,0,0.3);
-  padding: 40px;
+  box-shadow: 0px 1px 2px 2px rgba(0, 0, 0, 0.1);
+  padding: 70px;
   margin-bottom: 30px;
+  border-radius: 3px;
+  @media only screen and (min-width: 1200px) {
+    padding: 70px 100px 70px 100px;
+  }
 `;
 
 const FormHeader = styled(Header) `
   position: relative;
   padding-right: 2.5rem !important;
-  &::after{
+  @media only screen and (max-width: 991px) {
+    padding-bottom: 2rem !important;
+  }
+  &::after {
     position: absolute;
-    display: block;
-    right: 2rem;
+    display: inline-block;
     content: "";
     height: 20px;
-    border-right: 3px solid #191919;
-    top: 50%;
-    transform: translateY(-50%);
+    width: 100%;
+    border-bottom: 3px solid ${StyleGuide.color.geyScale.scale5};
+    bottom: 10px;
+    left: 0;
+
+    @media only screen and (min-width: 992px) {
+      width: 1px;
+      display: block;
+      position: absolute;
+      right: 2rem;
+      top: 50%;
+      left: initial;
+      bottom: initial;
+      transform: translateY(-50%);
+      border-bottom: 0;
+      border-right: 3px solid #191919;
+    }
   }
 `;
 
@@ -81,7 +101,6 @@ class CreateGroup extends Component {
     console.log(data);
     this.props.CreateNewGroupRequest(data, this.props.token).then(data => {
       console.log("CreateNewGroupRequest", data);
-      // this.props.history.push(`/groupDetail/${data.id}/group/null`)
       this.props.history.push(`/groupDetail/${data.id}`);
     });
   }
@@ -95,19 +114,13 @@ class CreateGroup extends Component {
           <ValidateForm onSubmit={this.onSubmitForm}>
             <FromFieldCard>
               <Grid>
-                <Grid.Column width={4}>
+                <Grid.Column mobile={16} computer={4}>
                   <FormHeader as="h2">그룹 정보</FormHeader>
                 </Grid.Column>
-                <Grid.Column width={12}>
-                  <Form.Group widths="equal">
+                <Grid.Column mobile={16} computer={12}>
                     <FormField name="title" label="그룹 이름" type="text" placeholder="그룹의 이름을 입력해주세요." validates={["required"]} RenderComponent={FormInput} />
-                  </Form.Group>
-                  <Form.Group widths="equal">
                     <FormField name="explanation" label="그룹 설명" placeholder="그룹 설명을 입력해주세요." RenderComponent={FormTextArea} />
-                  </Form.Group>
-                  <Form.Group widths="equal">
-                    <FormField name="thumbnail" label="썸네일 등록" placeholder="섬네일을 등록해주세요." RenderComponent={FormFile} validates={["required", "ThumbnailSize"]} />
-                  </Form.Group>
+                    <FormField name="thumbnail" label="썸네일 등록" placeholder="썸네일을 등록해주세요." RenderComponent={FormFile} validates={["required", "ThumbnailSize"]} />
                 </Grid.Column>
               </Grid>
             </FromFieldCard>
