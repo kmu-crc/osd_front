dc857aa9fab5c792cd331eda54854f8de3abca43
seoyeonyeom <sy8095@gmail.com>
2018-06-18 17:46:22 +0900

    time format 에 minute 정보도 추가

--- src/modules/DateFormat/DateFormat.js
+++ src/modules/DateFormat/DateFormat.js
@@ -2,12 +2,15 @@ const DateFormat = (date) => {
   const update = new Date(date);
   const today = new Date();
   const diff = today - update;
+  const diffMin = parseInt((diff / 1000) / 3600 * 60, 10); // N분 전
   const diffHour = parseInt((diff / 1000) / 3600, 10); // N시간 전
   const diffDay = parseInt(diffHour / 24, 10); // N일 전
 
   const m = 30;
-
-  if (diffDay < 1) { // 하루 전까지
+  
+  if (diffHour < 1) { // 한시간 전까지
+    return `${diffMin}분 전`;
+  } else if (1 <= diffHour && diffDay < 1) { // 하루 전까지
     return `${diffHour}시간 전`;
   } else if (1 <= diffDay && diffDay < m) { // 한달 전까지
     return `${diffDay}일 전`;
