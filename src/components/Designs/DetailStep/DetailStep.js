26cf7caae7b1185b542de924bcfd34ac537e2706
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-25 04:48:55 +0900

    boarder scoll 추가

--- src/components/Designs/DetailStep/DetailStep.js
+++ src/components/Designs/DetailStep/DetailStep.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import DesignStepCard from "components/Designs/DesignStepCard";
-import { SortablePane, Pane } from "react-sortable-pane";
 import StyleGuide from "StyleGuide";
 import { Grid, Icon } from "semantic-ui-react";
 import DesignBoardContainer from "containers/Designs/DesignBoardContainer";
@@ -22,18 +20,6 @@ const Board = styled.div`
   box-sizing: border-box;
   padding-left: 0 !important;
   padding-right: 0 !important;
-`;
-
-const EmptyCard = styled.div`
-  width: 100%;
-  min-height: 80px;
-  background-color: #fff;
-  border-radius: 3px;
-  box-shadow: 0 1px 5px #999898;
-  margin-top: 5px;
-  padding: 10px 5px;
-  cursor: pointer;
-  font-size: 12px;
 `;
 
 const BoardMask = styled.div`
@@ -120,10 +106,6 @@ class DetailStep extends Component {
       scroll += 264;
     }
     this.boardMask._reactInternalFiber.child.stateNode.scrollLeft = scroll;
-    console.log(
-      this.boardMask._reactInternalFiber.child.stateNode.scrollLeft,
-      scroll
-    );
 
     if (value) {
       if (scroll > 0) {
@@ -147,7 +129,6 @@ class DetailStep extends Component {
 
   scrollEvent = () => {
     const scroll = this.boardMask._reactInternalFiber.child.stateNode.scrollLeft;
-    console.log(scroll);
     if(scroll === 0){
       this.setState({ left: false, right: true });
     } else {
