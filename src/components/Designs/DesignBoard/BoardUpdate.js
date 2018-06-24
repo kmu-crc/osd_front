93cab820969d0be06aa4abeb6813ec19ae72482d
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-24 16:32:58 +0900

    close버튼 추가

--- src/components/Designs/DesignBoard/BoardUpdate.js
+++ src/components/Designs/DesignBoard/BoardUpdate.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 import ValidateForm from "components/Commons/ValidateForm";
+import { Icon } from "semantic-ui-react";
 import { FormInput } from "components/Commons/FormItem";
 import Button from "components/Commons/Button";
 import FormDataToJson from "modules/FormDataToJson";
 import styled from "styled-components";
 
 const Update = styled.div`
-  input {
-    width: 70% !important;
-    float: left;
-  }
+  padding: 10px 0;
   button {
-    width: 29%;
     margin: 0 !important;
     margin-left: 1% !important;
-    display: block !important;
-    float: left;
     line-height: 1.21428571em !important;
     padding: 0.67857143em 1em !important;
   }
@@ -26,21 +21,48 @@ const Update = styled.div`
   }
 `;
 
+const ButtonWrap = styled.div`
+  margin-top: 1rem;
+`;
+
+const CloseBtn = styled(Button)`
+  background-color: transparent !important;
+  border: 0 !important;
+  padding: 10px !important;
+  margin-left: 10px !important;
+`;
+
 class BoardUpdate extends Component {
   onSubmit = data => {
     let formData = FormDataToJson(data);
-    this.props.onUpdate(this.props.board.uid, this.props.token, formData).then(() => {
-      this.props.getBoard(this.props.board.design_id);
-      this.props.ModifyComplete();
-    })
+    this.props
+      .onUpdate(this.props.board.uid, this.props.token, formData)
+      .then(() => {
+        this.props.getBoard(this.props.board.design_id);
+        this.props.ModifyComplete();
+      });
+  };
+
+  handelClose = () => {
+    this.props.ModifyComplete();
   };
 
   render() {
     return (
       <Update>
         <ValidateForm onSubmit={this.onSubmit}>
-          <FormInput name="title" type="text" value={this.props.value} placeholder="새 보드 추가" />
-          <Button type="submit">수정</Button>
+          <FormInput
+            name="title"
+            type="text"
+            value={this.props.value}
+            placeholder="새 보드 추가"
+          />
+          <ButtonWrap>
+            <Button type="submit">수정</Button>
+            <CloseBtn type="button" onClick={this.handelClose}>
+              <Icon name="close" />
+            </CloseBtn>
+          </ButtonWrap>
         </ValidateForm>
       </Update>
     );
