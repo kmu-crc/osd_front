2ae1b8ad8130b109055cefa1a2998dc2ffc634fa
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-07-01 23:32:10 +0900

    config 수정

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const build = true;
 if(build) {
   host = "http://ec2-13-124-212-254.ap-northeast-2.compute.amazonaws.com";
 } else {
-  host = "http://localhost:3000";
+  host = "http://localhost:8080";
 }
 
 export default host;
