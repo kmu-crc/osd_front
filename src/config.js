63fa552269d59c0f650fb3c609532a1b6b835938
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-07-25 15:19:54 +0900

    build 환경 추가

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 let host = null;
 if(process.env.REACT_APP_BUILD === "true") {
   host = process.env.REACT_APP_API_URL;
-} else {
+} else if(process.env.REACT_APP_DEV) {
   host = process.env.REACT_APP_DEV_API_URL;
+} else if(process.env.REACT_APP_LOCAL) {
+  host = process.env.REACT_APP_LOCAL_API_URL;
 }
 
 export default host;
