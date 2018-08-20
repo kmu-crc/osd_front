ee57984f8becccbb86c7acebc543802a1c3615e6
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-08-21 04:08:11 +0900

    카드, 보드 생성하는 form 변경

--- src/components/Designs/CreateCard/CreateCard.js
+++ src/components/Designs/CreateCard/CreateCard.js
@@ -1,62 +1,106 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import ValidateForm from "components/Commons/ValidateForm";
-import { FormInput } from "components/Commons/FormItem";
 import { Button, Icon } from "semantic-ui-react";
-import FormDataToJson from "modules/FormDataToJson"
+import { FormInput } from "components/Commons/FormItems";
+import { ValidationGroup } from "modules/FormControl";
 
 const Title = styled.div`
   font-size: 1em;
   font-weight: 600;
-  padding: .67857143em 1em;
+  padding: 0.67857143em 1em;
   cursor: pointer;
   /* background-color: white; */
-`
+`;
 
 const ButtonWrap = styled.div`
   margin-top: 1rem;
-`
+`;
 
 const CloseBtn = styled(Button)`
   background-color: transparent !important;
   border: 0 !important;
   padding: 10px !important;
   margin-left: 10px !important;
-`
+`;
+
+const FInput = styled.div`
+  & > div {
+    margin-bottom: 1.5rem;
+  }
+`;
 
 class CreateCard extends Component {
   state = {
     active: false
-  }
+  };
   formActive = async () => {
-    await this.setState({ active: true } );
-    this.form._reactInternalFiber.child.child.stateNode.title.focus();
-  }
+    await this.setState({ active: true });
+    setTimeout(() => {
+      this.state.title.target.focus();
+    }, 100);
+  };
 
-  handleSubmit = (data) => {
-    let formData = FormDataToJson(data);
-    formData.order = this.props.lastOrder;
-    this.props.CreateDesignCardRequest(formData, this.props.designId, this.props.boardId, this.props.token).then(
-      this.props.GetDesignBoardRequest(this.props.designId)
-    );
-    this.setState({ active: false });
-  }
+  onChangeValue = async data => {
+    let obj = {};
+    if (data.target) {
+      obj[data.target.name] = data;
+    }
+    await this.setState(obj);
+    console.log(this.state);
+  };
 
-  handelClose = () => {
-    this.setState({ active: false });
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    let data = { ...this.state };
+    data.title.order = this.props.lastOrder;
+    ValidationGroup(data, true)
+      .then(data => {
+        this.props
+          .CreateDesignCardRequest(
+            data,
+            this.props.designId,
+            this.props.boardId,
+            this.props.token
+          )
+          .then(this.props.GetDesignBoardRequest(this.props.designId));
+        this.setState({ active: false });
+      })
+      .catch(console.log("실패"));
+  };
+
+  handelClose = (e) => {
+    if(e.type === "blur" && !this.form.contains(
+      e.relatedTarget
+    )){
+      this.setState({ active: false });
+    } else if(e.type === "click") {
+      this.setState({ active: false });
+    }
+  };
   render() {
-    return(
+    return (
       <div>
-        {this.state.active
-        ? <ValidateForm onSubmit={this.handleSubmit} ref={ ref => this.form = ref }>
-          <FormInput name="title" type="text" placeholder="새로운 카드의 제목을 입력해주세요."/>
-          <ButtonWrap>
-            <Button type="submit">생성</Button>
-            <CloseBtn type="button" onClick={this.handelClose} ><Icon name="close"/></CloseBtn>
-          </ButtonWrap>
-        </ValidateForm>
-        : <Title onClick={this.formActive}>카드 추가하기 +</Title>}
+        {this.state.active ? (
+          <form onSubmit={this.handleSubmit} ref={ref => (this.form = ref)} tabIndex="1" onBlur={this.handelClose}>
+            <FInput>
+              <FormInput
+                name="title"
+                placeholder="새로운 컨텐츠의 제목을 입력해주세요."
+                getValue={this.onChangeValue}
+                validates={["Required"]}
+              />
+            </FInput>
+
+            <ButtonWrap>
+              <Button type="submit">생성</Button>
+              <CloseBtn type="button" onClick={this.handelClose}>
+                <Icon name="close" />
+              </CloseBtn>
+            </ButtonWrap>
+          </form>
+        ) : (
+          <Title onClick={this.formActive}>컨텐츠 추가하기 +</Title>
+        )}
       </div>
     );
   }
