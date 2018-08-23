b0db685a1139f7079b558a38b24d4759dbdd5146
seoyeonyeom <sy8095@gmail.com>
2018-08-23 13:18:20 +0900

    수정버튼은 모달로 집어넣음 (디자인처럼)

--- src/components/Groups/GroupDetailNew/GroupDetailNew.js
+++ src/components/Groups/GroupDetailNew/GroupDetailNew.js
@@ -10,6 +10,7 @@ import Button from "components/Commons/Button";
 import eximg from "source/myPage.jpeg";
 import ContentBox from "components/Commons/ContentBox";
 import StyleGuide from "StyleGuide";
+import PxtoRem from "modules/PxtoRem";
 
 // css styling
 
@@ -60,10 +61,62 @@ const BtnContainer = styled.div`
   }
 `;
 
+const SideMenuBtn = styled.div`
+  position: absolute;
+  right: 0;
+  top: -5px;
+  width: ${PxtoRem(50)};
+  font-size: 1rem;
+  & > button {
+    line-height: ${PxtoRem(60)};
+    background-color: transparent;
+    border: 0;
+    width: ${PxtoRem(50)};
+    text-align: right;
+    padding: 0;
+    outline: 0;
+    i.icon {
+      margin-right: 0;
+      &::after {
+        margin-right: 0;
+      }
+    }
+  }
+`;
+
+const SideMenu = styled.ul`
+  display: block;
+  position: absolute;
+  width: ${PxtoRem(150)};
+  text-align: center;
+  bottom: 0;
+  right: 0;
+  z-index: 2;
+  transform: translateY(100%);
+  background-color: white;
+  box-shadow: 0px 2px 10px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  li {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    &:last-child {
+      border: 0;
+    }
+    & button {
+      width: 100%;
+      line-height: ${PxtoRem(45)};
+      background-color: transparent;
+      text-align: center;
+      border: 0;
+      padding: 0;
+      outline: 0;
+    }
+  }
+`;
+
 class GroupDetailNew extends Component {
   state = {
     editMode: false,
-    joinEditMode: false
+    activeMoreBtn: false
   };
 
   componentWillMount() {
@@ -104,9 +157,48 @@ class GroupDetailNew extends Component {
     }
   }
 
+  onActiveMoreBtn = e => {
+    if (e.type === "click") {
+      this.setState({
+        activeMoreBtn: !this.state.activeMoreBtn
+      });
+    }
+  };
+
+  onCloseMoreBtn = e => {
+    if (
+      e.type === "blur" &&
+      !this.MoreBtn._reactInternalFiber.child.stateNode.contains(
+        e.relatedTarget
+      )
+    ) {
+      this.setState({
+        activeMoreBtn: false
+      });
+    }
+    if (e.type === "click") {
+      this.setState({
+        activeMoreBtn: false
+      });
+    }
+  };
+
   render(){
     const groupDetail = this.props.GroupDetail;
     const user = this.props.userInfo;
+
+    const SubMenuCompo = () => {
+      return (
+        <SideMenu>
+          <li>
+            <Link to={`/groupDetail/${this.props.id}/modify`}>
+              <button>수정</button>
+            </Link>
+          </li>
+        </SideMenu>
+      );
+    };
+
     return(
       <div>
         {groupDetail.length !== 0 &&
@@ -114,7 +206,19 @@ class GroupDetailNew extends Component {
             <Wrapper>
             {/* ------------------------ 상단 프로필 섹션 -------------------------- */}
             <InfoContainer>
-              <Grid.Row className="title">{groupDetail.title}</Grid.Row>
+              <Grid.Row className="title">
+                <span>{groupDetail.title}</span>
+                {user && (user.uid === groupDetail.user_id) &&
+                  <SideMenuBtn tabIndex="1"
+                               onBlur={this.onCloseMoreBtn}
+                               ref={ref => (this.MoreBtn = ref)}>
+                    <button onClick={this.onActiveMoreBtn}>
+                      <Icon name="ellipsis vertical" />
+                    </button>
+                    {this.state.activeMoreBtn? <SubMenuCompo /> : null}
+                  </SideMenuBtn>
+                  }
+              </Grid.Row>
               <Grid.Row columns={2}>
                 <Grid.Column width={9}>
                   <div className="explanation">{groupDetail.explanation}</div>
@@ -122,22 +226,15 @@ class GroupDetailNew extends Component {
                   <div className="owner">그룹장 : {groupDetail.userName}</div>
                 </Grid.Column>
                 <Grid.Column className="btnWrap" width={7}>
-                  {user && (user.uid === groupDetail.user_id) &&
-                  <div className="btnContainer">
-                    <Link to={`/groupDetail/${this.props.id}/modify`}>
-                      <Button className="edit" size="small">수정</Button>
-                    </Link>
-                    <Button className="edit" size="small" onClick={()=>this.setState({editMode: !this.state.editMode})}>관리</Button>
-                  </div>
-                  }
+
                   <BtnContainer>
                     {this.props.like === true
                     ? <Button color="Primary" onClick={this.updateLike}>좋아요 취소 ({this.props.Count.like})</Button>
                     : <Button className="likeBtn" onClick={this.updateLike}>좋아요 ({this.props.Count.like})</Button>
                     }
                     {user && (user.uid === groupDetail.user_id)
-                    ? <Button className="edit" color="Solid" onClick={()=>this.setState({joinEditMode: !this.state.joinEditMode})}>
-                        가입 신청 관리
+                    ? <Button className="edit" color="Solid" onClick={()=>this.setState({editMode: !this.state.editMode})}>
+                        {this.state.editMode? "확인" : "관리"}
                       </Button>
                     : <JoinGroupContainer/>
                     }
@@ -146,10 +243,11 @@ class GroupDetailNew extends Component {
               </Grid.Row>
             </InfoContainer>
             {/* ------------------------ 하단 카드 렌더링 섹션 -------------------------- */}
-            {this.state.joinEditMode
-            ? <ModifyJoinListNew {...this.props}/>
-            : this.state.editMode
-            ? <ModifyExistListNew {...this.props}/>
+            {this.state.editMode
+            ? <div>
+                <ModifyJoinListNew {...this.props}/>
+                <ModifyExistListNew {...this.props}/>
+              </div>
             : <CurrentJoinListNew {...this.props}/>
             }
             </Wrapper>
