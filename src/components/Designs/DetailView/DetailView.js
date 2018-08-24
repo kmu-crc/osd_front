cdc28e5ee7928e91dddfce66fede86692cb32ad3
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-08-24 17:53:36 +0900

    업로드 로직 변경

--- src/components/Designs/DetailView/DetailView.js
+++ src/components/Designs/DetailView/DetailView.js
@@ -72,76 +72,114 @@ const CommentContainer = styled.div`
 `;
 
 const GoStepBtn = styled(Button)`
-  position: absolute;
+  margin-left: 0.5rem;
+  /* position: absolute;
   top: 0px;
-  right: 1rem;
-  // background-color: #57BBBA;
-  // border: 1px solid #57BBBA;
+  right: 1rem; */
+  /* background-color: #57BBBA;
+  border: 1px solid #57BBBA; */
 `;
 
+const BtnWrap = styled.div`
+  position: absolute;
+  top: 0;
+  right: 1rem;
+`;
 
 class DetailView extends Component {
   state = {
-    render: true
-  }
+    render: true,
+    edit: false
+  };
 
   componentDidMount() {
-    this.props.GetDesignDetailViewRequest(this.props.id, this.props.token)
-    .then(data => {
-      if (data.DesignDetailView !== null) {
-        this.props.GetCardCommentRequest(this.props.id, data.DesignDetailView.uid)
-      }
-    });
+    this.props
+      .GetDesignDetailViewRequest(this.props.id, this.props.token)
+      .then(data => {
+        if (data.DesignDetailView !== null) {
+          this.props.GetCardCommentRequest(
+            this.props.id,
+            data.DesignDetailView.uid
+          );
+        }
+      });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.props.DesignDetailViewResetRequest();
   }
 
   onActiveStep = () => {
-    const confirm = window.confirm("프로젝트 형식으로 변경하시겠습니까? 템플릿 변경 후에는 이전으로 돌아갈 수 없습니다. (현재 등록된 디자인은 저장됩니다)");
+    const confirm = window.confirm(
+      "프로젝트 형식으로 변경하시겠습니까? 템플릿 변경 후에는 이전으로 돌아갈 수 없습니다. (현재 등록된 디자인은 저장됩니다)"
+    );
     if (confirm) {
-      this.props.ChangeToProjectRequest(this.props.id, this.props.token)
-      .then(data => {
-        if (data.success === true) {
-          this.props.history.go(`/designDetail/${this.props.id}`);
-        }
-      });
+      this.props
+        .ChangeToProjectRequest(this.props.id, this.props.token)
+        .then(data => {
+          if (data.success === true) {
+            this.props.history.go(`/designDetail/${this.props.id}`);
+          }
+        });
     } else {
       return;
     }
-  }
+  };
 
-  onSubmitCmtForm = async (data) => {
+  onSubmitCmtForm = async data => {
     if (!this.props.token) {
       alert("로그인을 해주세요.");
       return;
     }
-    this.props.CreateCardCommentRequest(FormDataToJson(data), this.props.id, this.props.DesignDetailView.uid, this.props.token)
-    .then(async res => {
-      if (res.data.success === true) {
-        this.props.GetCardCommentRequest(this.props.id, this.props.DesignDetailView.uid);
-      }
-      await this.setState({
-        render: false
+    this.props
+      .CreateCardCommentRequest(
+        FormDataToJson(data),
+        this.props.id,
+        this.props.DesignDetailView.uid,
+        this.props.token
+      )
+      .then(async res => {
+        if (res.data.success === true) {
+          this.props.GetCardCommentRequest(
+            this.props.id,
+            this.props.DesignDetailView.uid
+          );
+        }
+        await this.setState({
+          render: false
+        });
+        this.setState({
+          render: true
+        });
       });
-      this.setState({
-        render: true
+  };
+
+  deleteComment = id => {
+    this.props
+      .DeleteCardCommentRequest(
+        this.props.id,
+        this.props.DesignDetailView.uid,
+        id,
+        this.props.token
+      )
+      .then(res => {
+        if (res.data.success === true) {
+          this.props.GetCardCommentRequest(
+            this.props.id,
+            this.props.DesignDetailView.uid
+          );
+        }
       });
-    });
-  }
+  };
 
-  deleteComment = (id) => {
-    this.props.DeleteCardCommentRequest(this.props.id, this.props.DesignDetailView.uid, id, this.props.token)
-    .then(res => {
-      if (res.data.success === true) {
-        this.props.GetCardCommentRequest(this.props.id, this.props.DesignDetailView.uid);
-      }
-    });
-  }
+  onChangeEditMode = () => {
+    this.setState({ edit: true });
+  };
+  onCloseEditMode = () => {
+    this.setState({ edit: false });
+  };
 
-
-  render(){
+  render() {
     const view = this.props.DesignDetailView;
     const len = Object.keys(view).length;
     const comment = this.props.Comment;
@@ -149,53 +187,85 @@ class DetailView extends Component {
     const CommentForm = () => {
       return (
         <ValidateForm onSubmit={this.onSubmitCmtForm} className="ui reply form">
-          <FormField name="comment" validates={["required"]} RenderComponent={FormTextArea} />
+          <FormField
+            name="comment"
+            validates={["required"]}
+            RenderComponent={FormTextArea}
+          />
           <Button type="submit" className="ui icon primary left labeled button">
-            <i aria-hidden="true" className="edit icon"></i>
+            <i aria-hidden="true" className="edit icon" />
             댓글쓰기
           </Button>
         </ValidateForm>
       );
-    }
+    };
 
-    return(
+    return (
       <div>
-        {len > 0 ?
+        <BtnWrap>
+          {this.props.isTeam && !this.state.edit ? (
+            <Button type="button" size="small" onClick={this.onChangeEditMode}>
+              컨텐츠 수정
+            </Button>
+          ) : null}
+          {this.props.token &&
+            this.props.userInfo.uid === view.user_id && (
+              <GoStepBtn onClick={this.onActiveStep} size="small">
+                단계 추가
+              </GoStepBtn>
+            )}
+        </BtnWrap>
+
+        {len > 0 ? (
           <ViewWrapper>
-            <div className="date">최근 업데이트 {(view.update_time).split("T")[0]}</div>
-            <CardSourceContainer view={this.props.DesignDetailView}/>
+            <div className="date">
+              최근 업데이트 {view.update_time.split("T")[0]}
+            </div>
+            <CardSourceContainer
+              view={this.props.DesignDetailView}
+              edit={this.state.edit}
+              closeEdit={this.onCloseEditMode}
+              openEdit={this.onChangeEditMode}
+            />
             <CommentContainer className="ui comments">
               <h4>댓글</h4>
-              {comment.length > 0?
-              comment.map(comm=>(
-                <div className="comment" key={comm.uid}>
-                  <div className="avatar">
-                    <img src={comm.s_img? comm.s_img : eximg} alt="profile" />
-                  </div>
-                  <div className="content">
-                    <a className="author">{comm.nick_name}</a>
-                    <div className="metadata">
-                      <div>{comm.create_time.split("T")[0]}</div>
+              {comment.length > 0 ? (
+                comment.map(comm => (
+                  <div className="comment" key={comm.uid}>
+                    <div className="avatar">
+                      <img
+                        src={comm.s_img ? comm.s_img : eximg}
+                        alt="profile"
+                      />
                     </div>
-                    <div className="text">{comm.comment}</div>
+                    <div className="content">
+                      <a className="author">{comm.nick_name}</a>
+                      <div className="metadata">
+                        <div>{comm.create_time.split("T")[0]}</div>
+                      </div>
+                      <div className="text">{comm.comment}</div>
+                    </div>
+                    {this.props.userInfo &&
+                      this.props.userInfo.uid === comm.user_id && (
+                        <Button
+                          size="small"
+                          className="delBtn"
+                          onClick={() => this.deleteComment(comm.uid)}
+                        >
+                          삭제
+                        </Button>
+                      )}
                   </div>
-                  {this.props.userInfo && this.props.userInfo.uid === comm.user_id &&
-                  <Button size="small" className="delBtn" onClick={()=>this.deleteComment(comm.uid)}>삭제</Button>
-                  }
-                </div>
                 ))
-              :
-              <p>등록된 코멘트가 없습니다.</p>
-              }
-              {this.state.render? <CommentForm/> : null}
+              ) : (
+                <p>등록된 코멘트가 없습니다.</p>
+              )}
+              {this.state.render ? <CommentForm /> : null}
             </CommentContainer>
           </ViewWrapper>
-        :
-        <Loading/>
-        }
-        {this.props.token && this.props.userInfo.uid === view.user_id &&
-          <GoStepBtn onClick={this.onActiveStep} size="small">단계 추가</GoStepBtn>
-        }
+        ) : (
+          <Loading />
+        )}
       </div>
     );
   }
