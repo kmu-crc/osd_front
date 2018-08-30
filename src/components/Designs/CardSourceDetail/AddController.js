a3f0fbe3d0d26d67ef5a4bab67e6ce09b14ea449
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-08-30 09:23:10 +0900

    addcontroller에 filecontroller직접 추가

--- src/components/Designs/CardSourceDetail/AddController.js
+++ src/components/Designs/CardSourceDetail/AddController.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import StyleGuide from "StyleGuide";
 import { Icon } from "semantic-ui-react";
+import FileController from "./FileController";
 
 const ControllerWrap = styled.div`
   padding: 20px 0;
@@ -56,14 +57,35 @@ const ControllerMenu = styled.ul`
 `;
 
 class AddController extends Component {
-
+  state = {
+    type: null,
+    content: "",
+    order: null
+  }
   addContent = async (type) => {
-    await this.setState({type, order:this.props.order, content: ""});
-    this.returnData();
+    if(type === "FILE"){
+      await this.setState({type, order:this.props.order, content: "", initClick: true});
+      // 처음 fileController가 동작하기 위해서는 initClick가 true여야 하지만 한번 동작한 후에는 false 바뀌어야
+      // 최종적으로 저장을 눌렀을때 파일 선택창이 반복해서 뜨지않는다.
+      // 조금 걱정되는것은 현재 타이머를 걸어 0.1초 뒤에 initClick를 false로 바꿔주게 해놨는데
+      // 나중에 컴퓨터사양이 느리거나 인터넷이 느린곳에서 오작동할 우려가 있다....
+      // 좀 더 지켜봐야할 것 같다.
+      setTimeout(() => {
+        this.setState({initClick: false});
+      }, 100);
+    } else {
+      await this.setState({type, order:this.props.order, content: ""});
+      this.returnData();
+    }
   }
 
-  returnData = async () => {
-    if(this.props.getValue) this.props.getValue(this.state);
+  returnData = async (data) => {
+    if(data){
+      await this.setState({type: null, order:this.props.order, content: ""})
+      this.props.getValue(data);
+    } else {
+      if(this.props.getValue) this.props.getValue(this.state);
+    }
   }
   render() {
     return (
@@ -84,6 +106,7 @@ class AddController extends Component {
               EMBED
             </li> */}
           </ControllerMenu>
+          {this.state.type === "FILE" && <FileController item={this.state} getValue={this.returnData}/>}
         </div>
       </ControllerWrap>
     );
