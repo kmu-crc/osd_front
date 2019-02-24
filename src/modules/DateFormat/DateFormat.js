56d10e8bddff40eeb558a8a21b7f22f111552c7e
ggamsang <w.ggmsng@gmail.com>
2019-02-24 17:39:43 +0900

    issue #99 dateformat

--- src/modules/DateFormat/DateFormat.js
+++ src/modules/DateFormat/DateFormat.js
@@ -3,12 +3,14 @@ const DateFormat = (date) => {
   let today = new Date();
    //today = today.getTime() + 32400000;
   today = today.getTime();
+
+  const m = 30;
   const diff = today - update;
   const diffMin = parseInt((diff / 1000) / 3600 * 60, 10); // N분 전
   const diffHour = parseInt((diff / 1000) / 3600, 10); // N시간 전
   const diffDay = parseInt(diffHour / 24, 10); // N일 전
+  const diffMon = parseInt(diffDay/m, 10);
 
-  const m = 30;
   if (diffMin == 0) {
     return `방금 전`;
   } else if (diffHour < 1) { // 한시간 전까지
@@ -17,9 +19,11 @@ const DateFormat = (date) => {
     return `${diffHour}시간 전`;
   } else if (1 <= diffDay && diffDay < m) { // 한달 전까지
     return `${diffDay}일 전`;
-  } else if (m <= diffDay) { // 한달 이후부터
-    return `${parseInt(diffDay/m, 10)}달 전`;
-  }
+  } else if (11 >= diffMon &&m <= diffDay) { // 한달 이후부터
+    return `${diffMon}달 전`;
+  } else if(12 <= diffMon){
+    return `${(diffMon/12.0).toFixed(1)}년 전`;
+  } 
 }
 
 export default DateFormat;
