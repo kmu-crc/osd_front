a19c3f195e6b7144826168ba9cbc07f81334a7f7
ggamsang <w.ggmsng@gmail.com>
2019-05-01 22:13:39 +0900

    ISSUE #194 - check designer is

--- src/containers/Designs/CreateDesignFormContainer/CreateDesignFormContainer.js
+++ src/containers/Designs/CreateDesignFormContainer/CreateDesignFormContainer.js
@@ -7,16 +7,33 @@ import { SearchMemberRequest } from "actions/Commons/Search";
 import { GetCategoryLevel2Request } from "actions/Categorys";
 
 class CreateDesignFormContainer extends Component {
+
+  componentDidMount() {
+    if (this.props.userInfo.is_designer === 0) {
+      alert("디자이너가 아닙니다. 개인정보 페이지에 가셔서 디자이너로 등록하여주세요.")
+      this.props.history.push("/myModify")
+    }
+  }
   render() {
+    console.log("props:", this.props.userInfo)
     return(
-      <CreateDesignForm {...this.props}/>
-    );
+      <div>{
+        this.props.userInfo.is_designer === 1 ? (
+          <CreateDesignForm {...this.props}/>
+        ) : (
+          <p style={{color:"#FFF"}}> 권한을 확인 중입니다.</p>
+        )
+      }</div>
+    )
   }
 }
+
+
 const mapStateToProps = (state) => {
   return {
     token: state.Authentication.status.token,
     members: state.Search.status.members,
+    userInfo: state.Authentication.status.userInfo,
     cate1: state.Categorys.status.level1,
     cate2: state.Categorys.status.level2
   };
