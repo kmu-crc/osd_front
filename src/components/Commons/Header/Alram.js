470b1e415c0b5f40006e1976ce1f93f867f6b6ed
ggamsang <w.ggmsng@gmail.com>
2019-05-09 15:12:27 +0900

    alarm remove all except specific alarm

--- src/components/Commons/Header/Alram.js
+++ src/components/Commons/Header/Alram.js
@@ -92,6 +92,14 @@ class Alram extends Component {
     this.props.socket.emit("confirm", { uid: this.props.uid, alramId: id });
   }
 
+  allAlarmConfirm = () => {
+    // console.log("ALL", this.props)
+    alert('초대받은 디자인 및 그룹에 대한 알람을 제외한 모든 알람들을 읽음으로 표시합니다.')
+    this.props.socket.emit("allConfirm", {user_id: this.props.uid})
+    console.log(this.props,"!!!")
+    this.props.getNoti
+  }
+
   getLink = item => {
     let link = ``;
     if ( item.type === "MESSAGE" ){
@@ -149,52 +157,6 @@ class Alram extends Component {
     return msg;
   }
 
-  getMessageText = item => {
-    let msg = ""
-    if(item.type === "MESSAGE"){
-      msg = "새 메시지가 도착했습니다."
-    } else if (item.type === "DESIGN") {
-      if(item.kinds === "INVITE") {
-        msg = "디자인 초대가 왔습니다."
-      } else if (item.kinds === "REQUEST"){
-        msg = "디자인 가입요청이 있습니다."
-      } else if (item.kinds === "INVITE_TRUE"){
-        msg = `${item.fromUser}님이 멤버가 되었습니다.`
-      } else if (item.kinds === "REQUEST_TRUE"){
-        msg = `${item.title}의 멤버가 되었습니다.`
-      } else if (item.kinds === "GETOUT"){
-        msg = `${item.title}디자인에서 탈퇴되셨습니다.`
-      } else if (item.kinds === "REFUSE"){
-        msg = `멤버요청을 거절하였습니다.`
-      } else if (item.kinds === "INVITE_REJECT"){
-        msg = `초대를 거절하였습니다.`
-      } else if (item.kinds === "LIKE") {
-        msg = `좋아요가 눌렸습니다.`
-      }
-    } else if (item.type === "GROUP") {
-      if(item.kinds === "JOIN"){
-        msg = `그룹에 새 가입요청이 있습니다.`
-      } else if(item.kinds === "JOINSUCCESS"){
-        msg = `그룹에 가입이 승인되었습니다.`
-      } else if(item.kinds === "JOINREFUSE"){
-        msg = `그룹활동이 거절되었습니다.`
-      }
-    }
-    /*{item.type === "MESSAGE" ? "새 메시지가 도착했습니다."
-    : item.type === "DESIGN"
-      ? item.kinds === "INVITE" ? `디자인 초대가 왔습니다.`
-      : item.kinds === "REQUEST" ? `디자인 가입요청이 있습니다.`
-      : item.kinds === "INVITE_TRUE" ? `${item.fromUser}님이 멤버가 되었습니다.`
-      : item.kinds === "REQUEST_TRUE" ? `${item.title}의 멤버가 되었습니다.`
-      : item.kinds === "REFUSE" ? `맴버요청을 거절하셨습니다.`
-      : item.kinds === "LIKE" ? "좋아요가 눌렸습니다." : ``
-    : item.type === "GROUP"
-      ? item.kinds === "JOIN" ? `그룹에 새 가입요청이 있습니다.`
-      : item.kinds === "JOINSUCCESS" ? "그룹에 가입이 승인되었습니다."
-      : item.kinds === "JOINREFUSE" ? "그룹활동을 거절당하였습니다." : ""
-    : null}*/
-    return msg;
-  }
   render() {
     return (
       <button
@@ -213,30 +175,23 @@ class Alram extends Component {
             { this.props.noti.list == null || this.props.noti.list.length === 0? (
               <AlarmItem>알람이 없습니다.</AlarmItem>
             ) : (
-              this.props.noti.list.map((item, index) => {
-                return (
-                  <AlarmItem
-                    key={index}
-                    className={item.confirm ? "confirm" : null}
-                    onClick={() => this.alramConfirm(item.uid)}
-                  >
-                    <Link onClick = {this.forceUpdate}
-                      to={this.getLink(item)}
-                    >
+              <div>
+                {this.props.noti.count>0&&<AlarmItem onClick={this.allAlarmConfirm}><h4>모두읽음표시</h4></AlarmItem>}
+              {this.props.noti.list.map((item, index) => {
+                  return (
+                  <AlarmItem key={index} className={item.confirm ? "confirm" : null} onClick={() => this.alramConfirm(item.uid)} >
+                    <Link onClick = {this.forceUpdate} to={this.getLink(item)}>
                       <h4><TextFormat txt={item.title}/></h4>
                       <TextFormat txt={this.getMessageText(item)}/>
-                      <span className="time">
-                        {DateFormat(item.create_time)}
-                      </span>
+                      <span className="time">{DateFormat(item.create_time)}</span>
                     </Link>
-                  </AlarmItem>
-                );
-              })
+                  </AlarmItem>)
+              })}</div>
             )}
           </AlarmDropDown>
         )}
       </button>
-    );
+    )
   }
 }
 
