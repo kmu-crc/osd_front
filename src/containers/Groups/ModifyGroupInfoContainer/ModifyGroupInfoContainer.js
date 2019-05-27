2c6c276a921aeeb1e2ab0aebff4034471b89c4ba
opensrcdesign-admin <opensrcdesign@gmail.com>
2019-05-27 14:26:07 +0900

    protect group modify from another users

--- src/containers/Groups/ModifyGroupInfoContainer/ModifyGroupInfoContainer.js
+++ src/containers/Groups/ModifyGroupInfoContainer/ModifyGroupInfoContainer.js
@@ -4,23 +4,40 @@ import { GetGroupDetailRequest, UpdateGroupRequest } from "actions/Group";
 import ModifyGroupInfo from "components/Groups/ModifyGroupInfo";
 
 class ModifyGroupInfoContainer extends Component {
-  componentDidMount(){
-    this.props.GetGroupDetailRequest(this.props.id, this.props.token);
+  state = {
+    isAuthor: false
+  }
+  componentDidMount() {
+    this.props.GetGroupDetailRequest(this.props.id, this.props.token)
+      .then(() => {
+        if (this.props.userInfo.uid !== this.props.GroupDetail.user_id) {
+          alert("이 그룹에 대한 수정권한이 없습니다. 이전페이지로 돌아갑니다.")
+          this.props.history.go(-1)
+        } else { this.setState({ isAuthor: true }) }
+      })
   }
 
   render() {
-    return(
-      <ModifyGroupInfo {...this.props}/>
-    );
+    return (
+      <div>
+        {this.state.isAuthor ? (
+          <ModifyGroupInfo {...this.props} />
+        ) : (
+            <p style={{ color: "#FFF" }}>수정권한을 확인 중 입니다.</p>
+          )}
+      </div>
+    )
   }
 }
+
 
 const mapStateToProps = (state) => {
   return {
     token: state.Authentication.status.token,
     GroupDetail: state.GroupDetail.status.GroupDetail,
-  };
-};
+    userInfo: state.Authentication.status.userInfo
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -30,7 +47,7 @@ const mapDispatchToProps = (dispatch) => {
     UpdateGroupRequest: (id, data, token) => {
       return dispatch(UpdateGroupRequest(id, data, token))
     }
-  };
-};
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModifyGroupInfoContainer);
