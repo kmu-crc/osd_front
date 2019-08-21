3be2256fea6c09726eea446c2d84ec850a483b3e
ggamsang <w.ggmsng@gmail.com>
2019-08-21 14:11:31 +0900

    signnav thumbnail, mypage thumbnail code changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,6 +18,7 @@ class Message extends Component {
     }
     render() {
         return (<div style={{ cursor: "pointer" }} onClick={this.gotoMessagePage}>
+            {this.props.countMsg > 0 && <div style={{ zIndex: "998", position: "absolute", marginLeft: "29px", width: "6px", height: "6px", borderRadius: "50%", backgroundColor: "red" }} />}
             <i style={{ zIndex: "997", opacity: ".9", fontSize: "34px" }} className="material-icons">email</i>
         </div>)
     }
