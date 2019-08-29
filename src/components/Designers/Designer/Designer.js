1f76bed9ca6f245f3c829093674ba69a52cde083
ggamsang <w.ggmsng@gmail.com>
2019-08-29 14:02:52 +0900

    modified css: designer component, modify design section

--- src/components/Designers/Designer/Designer.js
+++ src/components/Designers/Designer/Designer.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import noimg from "source/noimg.png"
-
+import noimg from "source/thumbnail.png"
 
 import iForked from "source/baseline_library_books_black_48dp.png"
 import iThumbUp from "source/thumbup_icon_black.png"
 import IconView from "source/IconView"
 
 //formats
-import NumberFormat from 'modules/formats/NumberFormat'
-import TextFormat from 'modules/formats/TextFormat'
+import NumberFormat from "modules/formats/NumberFormat"
+import TextFormat from "modules/formats/TextFormat"
+import DateFormat from "modules/DateFormat"
 //styled
 
 const DesignerComp = styled.div`
     font-family: Noto Sans KR;
-    height:150px;
-    width:587px;
-    
-    
+    height: 150px;
+    width: 587px;
+
     .ImageBox{
         width: 150px;
         height: 150px;
@@ -25,95 +24,111 @@ const DesignerComp = styled.div`
         border-radius: 50%;
         position: absolute;
         background-size:cover;
+        border: 1.5px solid #EFEFEF;
         & img {
           width: 100%;
           height: 100%;
         }
         z-index:1;
-        
     }
-    
     .TextBox{
         width: 527px;
         height: 130px;
-        background-color:#EFEFEF;
+        background-color: #EFEFEF;
         border-radius: 15px 15px 15px 15px;
         position: relative;
         left:65px;
         top:8px;
     }
     .userName{
-        top:19px;
-        left:114px;
-        font-size:20px;
-        position:absolute;
-        color:#707070;
+        top: 19px;
+        left: 114px;
+        font-size: 20px;
+        position: absolute;
+        color: #707070;
         font-weight: bold;
+        max-width: 300px;
     }
-
+    .update{
+        position: absolute;
+        top: 20px;
+        right: 10px;
+        width: 75px;
+        background-color: #EFEFEF;
+        color: #707070;
+        font-weight: light;
+        font-size: 15px;
+        text-align: right;
+    }
     .description{
-        top:56px;
-        left:114px;
-        font-size:20px;
-        max-height: 30px;
-        max-width:330px;
-        overflow:hidden;
-        white-space:nowrap;
-        text-overflow:ellipsis;
-        position:absolute;
-        color:#707070;
+        top: 56px;
+        left: 114px;
+        font-size: 20px;
+        line-height: 20px;
+        max-width: 385px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        position: absolute;
+        color: #707070;
         font-weight: 100;
     }
     .cate{
-        top:95px;
-        left:380px;
-        height:30px;
-        width:120px;
-        position:absolute;
-        color:#FF0000;
+        position: absolute;
+        top: 95px;
+        right: 10px;
+        height: 30px;
+        width: 190px;
+        color: #FF0000;
         font-weight: 300;
-        font-size:20px;
-        text-align:right;
-        
+        font-size: 20px;
+        text-align: right;
     }
-    
+    .counter{
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        display: flex;
+        margin-top: 95px;
+        margin-left: 110px;
+        justify-content: space-start;
+    }
 `;
 
-
-
-class Designer extends Component{
-    state = {data : this.props.data};
-
+class Designer extends Component {
+    state = { data: this.props.data };
     gotoDesignerDetailPage = () => {
         window.location.href = "/designerDetail/"
     }
-    render(){
-
+    render() {
         const designer = this.state.data;
-        return(
+        return (
             <>
                 <DesignerComp>
-                    <div onClick = {this.gotoDesignerDetailPage} className="ImageBox" style={designer.imgURL ? { backgroundImage: `url(${designer.imgURL.m_img})` } : { backgroundImage: `url(${noimg})` }}></div>
+                    <div onClick={this.gotoDesignerDetailPage} className="ImageBox" style={designer.imgURL ? { backgroundImage: `url(${designer.imgURL.m_img})` } : { backgroundImage: `url(${noimg})` }}></div>
                     <div className="TextBox">
                         <div className="userName">{designer.nick_name}</div>
-                        <div className="description"><TextFormat txt={designer.about_me}/></div>
+                        <div className="description"><TextFormat txt={designer.about_me} /></div>
+                        <div className="update">{DateFormat(designer.update_time)}</div>
                         <div className="cate">{designer.categoryName || "전체"}</div>
-                        <div style={{ display: "flex", justifyContent: "space-start", paddingTop:'100px'}}>
-                            <div className="view" style={{paddingLeft:"110px"}} ><IconView width="22px" height="11px"  fill="#000000" opacity="0.55"/></div>
-                            <div style={{paddingLeft:"5px",fontSize:'15px'}}>{NumberFormat(designer.total_view)}</div>
-                            <div className="like" style={{paddingLeft:"22px"}}><img alt="icon" src={iThumbUp}  style={{ width: "11px", height: "11px", opacity:"0.55"}}/></div>
-                            <div style={{paddingLeft:"5px",fontSize:'15px'}}>{NumberFormat(designer.total_like)}</div>
-                            <div className="child" style={{paddingLeft:"22px"}}><img alt="icon" src={iForked} style={{ width: "21px", height: "21px" , opacity:"0.55"}} /></div>
-                            <div style={{paddingLeft:"5px",fontSize:'15px'}}>{NumberFormat(designer.total_group)}</div>
+                        <div className="counter">
+                            <div className="view" style={{ display: "flex" }}>
+                                <div><IconView width="22px" height="11px" fill="#000000" opacity="0.55" /></div>
+                                <div style={{ width: "40px", fontSize: '15px' }}>{NumberFormat(designer.total_view)}</div>
+                            </div>
+                            <div className="like" style={{ display: "flex", marginLeft: "10px" }}>
+                                <div><img alt="icon" src={iThumbUp} style={{ width: "11px", height: "11px", opacity: "0.55" }} /></div>
+                                <div style={{ width: "40px", fontSize: '15px' }}>{NumberFormat(designer.total_like)}</div>
+                            </div>
+                            <div className="child" style={{ display: "flex", marginLeft: "10px" }}>
+                                <img alt="icon" src={iForked} style={{ width: "21px", height: "21px", opacity: "0.55" }} />
+                                <div style={{ width: "40px", fontSize: '15px' }}>{NumberFormat(designer.total_group)}</div>
+                            </div>
                         </div>
                     </div>
-
                 </DesignerComp>
-
             </>
-
         )
     }
-
 }
 export default Designer
--- src/components/Designs/ModifyDesign/ModifyDesign.js
+++ src/components/Designs/ModifyDesign/ModifyDesign.js
@@ -8,74 +8,63 @@ import { Modal } from "semantic-ui-react";
 
 const scrollmenu = [{ txt: "기본 정보", tag: "#basics" }, { txt: "부가 정보", tag: "#additional" }, { txt: "단계/컨텐츠 정보", tag: "#contenteditor" }]
 
-
-
-
-const modify_Main_Banner ={ width: "1920px", display: "flex", justifyContent: "center" }
+const modify_Main_Banner = { width: "1920px", display: "flex", justifyContent: "center" }
 const modify_Main_Banner_text = { marginTop: "45px", width: "196px", height: "37px", fontFamily: "Noto Sans KR", fontSize: "25px", fontWeight: "700", lineHeight: "37px", textAlign: "center", color: "#707070" }
 const modify_Main_Section = { display: "flex", marginTop: "60px", marginBottom: "111px" }
-const modify_Menu_Delete={position:"fixed", top:"429px",left:"100px",width:"150px",height:"29px",fontFamily: "Noto Sans KR",fontWeight:"500",fontSize:"20px",color:"#FF0000"}
+const modify_Menu_Delete = { position: "fixed", top: "429px", left: "100px", width: "150px", height: "29px", fontFamily: "Noto Sans KR", fontWeight: "500", fontSize: "20px", color: "#FF0000" }
 const modify_Btn_Back = { cursor: "pointer", width: "104.5px", height: "44px", borderRadius: "5px", backgroundColor: "#FF0000", paddingTop: "6px", paddingLeft: "15px", marginRight: "15px" }
-const modify_Btn_Back_text ={ width: "74px", padding: "0px", fontFamilty: "Noto Sans KR", fontWeight: "500", lineHeight: "29px", textAlign: "center", fontSize: "20px", color: "#FFFFFF" }
-const modify_Btn_Next ={ width: "74px", padding: "0px", fontFamilty: "Noto Sans KR", fontWeight: "500", lineHeight: "29px", textAlign: "center", fontSize: "20px", color: "#FFFFFF" }
+const modify_Btn_Back_text = { width: "74px", padding: "0px", fontFamilty: "Noto Sans KR", fontWeight: "500", lineHeight: "29px", textAlign: "center", fontSize: "20px", color: "#FFFFFF" }
+const modify_Btn_Next = { width: "74px", padding: "0px", fontFamilty: "Noto Sans KR", fontWeight: "500", lineHeight: "29px", textAlign: "center", fontSize: "20px", color: "#FFFFFF" }
 
 class ModifyDesign extends Component {
 
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
-    this.state = {isDelete:false,deleteDialog:false,thumbnail:"",designTitle:"",designExplain:"", loading: false, isPossibleNextStep: false, step: 0, /* 0: basics, 1: additional, 2: contents*/ selectedCate1: null,
-     selectedCate2: null, cate1: null,cate2: null}
+    this.state = {
+      isDelete: false, deleteDialog: false, thumbnail: "", designTitle: "", designExplain: "", loading: false, isPossibleNextStep: false, step: 0, /* 0: basics, 1: additional, 2: contents*/ selectedCate1: null,
+      selectedCate2: null, cate1: null, cate2: null
+    }
     this.handleInputDesignExplain = this.handleInputDesignExplain.bind(this);
     this.handleInputDesignTitle = this.handleInputDesignTitle.bind(this);
-    this.handleChangeThumbnail=this.handleChangeThumbnail.bind(this);
+    this.handleChangeThumbnail = this.handleChangeThumbnail.bind(this);
     this.handleOnClickDeleteDesign = this.handleOnClickDeleteDesign.bind(this);
   }
   // setLoader = () => { this.setState({ loading: !this.state.loading }) }
-  componentDidMount()
-  {
+  componentDidMount() {
   }
-  shouldComponentUpdate(nextProps)
-  {
-    console.log("TEST",nextProps.DesignDetail.title);
-      if(this.props.DesignDetail.title!=nextProps.DesignDetail.title) this.setState({designTitle:nextProps.DesignDetail.title});
-      if(this.props.DesignDetail.explanation!=nextProps.DesignDetail.explanation) this.setState({designExplain:nextProps.DesignDetail.explanation});
-      if(this.props.DesignDetail.img!=nextProps.DesignDetail.img) this.setState({thumbnail:nextProps.DesignDetail.img.m_img});
-    
-      return true;
+  shouldComponentUpdate(nextProps) {
+    console.log("TEST", nextProps.DesignDetail.title);
+    if (this.props.DesignDetail.title != nextProps.DesignDetail.title) this.setState({ designTitle: nextProps.DesignDetail.title });
+    if (this.props.DesignDetail.explanation != nextProps.DesignDetail.explanation) this.setState({ designExplain: nextProps.DesignDetail.explanation });
+    if (this.props.DesignDetail.img != nextProps.DesignDetail.img) this.setState({ thumbnail: nextProps.DesignDetail.img.m_img });
+
+    return true;
   }
-  handleOnClickDeleteDesign()
-  {
-      if(this.state.isDelete == true)
-      {
-        this.setState({ isDelete: !this.state.isDelete,deleteDialog:false })
-      }
-      else
-      {
-        this.setState({ isDelete: !this.state.isDelete, deleteDialog: true })     
-      }
-    
+  handleOnClickDeleteDesign() {
+    if (this.state.isDelete == true) {
+      this.setState({ isDelete: !this.state.isDelete, deleteDialog: false })
+    }
+    else {
+      this.setState({ isDelete: !this.state.isDelete, deleteDialog: true })
+    }
+
   }
-  handleInputDesignTitle(title)
-  {
+  handleInputDesignTitle(title) {
     console.log("titleinput");
-    this.setState(state=>({designTitle:title}))
+    this.setState(state => ({ designTitle: title }))
   }
-  handleInputDesignExplain(explain)
-  {
-    this.setState(state=>({designExplain:explain}))
+  handleInputDesignExplain(explain) {
+    this.setState(state => ({ designExplain: explain }))
   }
-  handleChangeThumbnail(imgurl)
-  {
-    this.setState(state=>({thumbnail:imgurl}));
+  handleChangeThumbnail(imgurl) {
+    this.setState(state => ({ thumbnail: imgurl }));
   }
-  
+
   checkIsPossibleToGoNextStep = (step) => {
 
   }
 
-  gotoStep=(menu,index)=>
-  {
+  gotoStep = (menu, index) => {
     this.setState({ step: index });
   }
   gotoPrevStep = () => {
@@ -93,58 +82,60 @@ class ModifyDesign extends Component {
   submit = () => {
     console.log("!!!");
   }
-
   completed = () => {
     this.setState({ isPossibleNextStep: true })
   }
+  preventEnterKey = (event) => {
+    if (event.charCode === 13) {
+      event.preventDefault()
+    }
+  }
   render() {
     // const myInfo = this.props.MyDetail
-
-
     const SectionContentEditor = () => {
-     
     }
     const { step } = this.state;
-    const DeleteDesignModal = ()=>
-    {
-      return(
-        <Modal open={this.state.deleteDialog} style={{boxShadow:"0px 3px 6px #000000",position:"fixed",width:"576px",height:"200px",textAlign:"center",top:"283px"}}>
-        <div style = {{width:"100%",height:"69px",fontFamily:"Noto Sans KR",fontSize:"20px",color:"#707070",lineHeight:"40px",marginTop:"35px",marginBottom:"31px"}}>{this.props.DesignDetail.title}<br/>삭제하시겠습니까?</div>
-        <div style = {{width:"100%",height:"29px",fontFamily:"Noto Sans KR",fontSize:"20px",color:"#707070",textDecoration:"underline",color:"#FF0000"}}>네, 삭제합니다</div>
-        <div onClick = {this.handleOnClickDeleteDesign} style={{position:"absolute",right:"-50px",top:"0px",width:"22px",height:"22px",
-                    backgroundImage: `url(${iDelete})`,backgroundSize: "cover", backgroundPosition: "center center",}}>
-        </div>
-      </Modal>
+    const DeleteDesignModal = () => {
+      return (
+        <Modal open={this.state.deleteDialog} style={{ boxShadow: "0px 3px 6px #000000", position: "fixed", width: "576px", height: "200px", textAlign: "center", top: "283px" }}>
+          <div style={{ width: "100%", height: "69px", fontFamily: "Noto Sans KR", fontSize: "20px", color: "#707070", lineHeight: "40px", marginTop: "35px", marginBottom: "31px" }}>{this.props.DesignDetail.title}<br />삭제하시겠습니까?</div>
+          <div style={{ width: "100%", height: "29px", fontFamily: "Noto Sans KR", fontSize: "20px", color: "#707070", textDecoration: "underline", color: "#FF0000" }}>네, 삭제합니다</div>
+          <div onClick={this.handleOnClickDeleteDesign} style={{
+            position: "absolute", right: "-50px", top: "0px", width: "22px", height: "22px",
+            backgroundImage: `url(${iDelete})`, backgroundSize: "cover", backgroundPosition: "center center",
+          }}>
+          </div>
+        </Modal>
       );
     }
-    const DeleteWariningModal = ()=>
-    {
-      return(
-        <Modal open={this.state.deleteDialog} style={{boxShadow:"0px 3px 6px #000000",position:"fixed",width:"576px",height:"160px",textAlign:"center",top:"40px"}}>
-        <div style = {{width:"100%",height:"29px",fontFamily:"Noto Sans KR",fontSize:"20px",color:"#707070",lineHeight:"29px",marginTop:"40px",marginBottom:"10px"}}>
-          사용자 매뉴얼 디자인 등록 01을 삭제하지 못했습니다.</div>
-        <div style = {{width:"100%",height:"29px",fontFamily:"Noto Sans KR",fontSize:"20px",textDecoration:"none",color:"#FF0000"}}>
-          컨텐츠의 개설자만 삭제할 권한이 주어집니다.</div>
-      </Modal>
+    const DeleteWariningModal = () => {
+      return (
+        <Modal open={this.state.deleteDialog} style={{ boxShadow: "0px 3px 6px #000000", position: "fixed", width: "576px", height: "160px", textAlign: "center", top: "40px" }}>
+          <div style={{ width: "100%", height: "29px", fontFamily: "Noto Sans KR", fontSize: "20px", color: "#707070", lineHeight: "29px", marginTop: "40px", marginBottom: "10px" }}>
+            사용자 매뉴얼 디자인 등록 01을 삭제하지 못했습니다.</div>
+          <div style={{ width: "100%", height: "29px", fontFamily: "Noto Sans KR", fontSize: "20px", textDecoration: "none", color: "#FF0000" }}>
+            컨텐츠의 개설자만 삭제할 권한이 주어집니다.</div>
+        </Modal>
       );
     }
-    const DeleteCompleteModal = ()=>
-    {
-      return(
-        <Modal open={this.state.deleteDialog} style={{boxShadow:"0px 3px 6px #000000",position:"fixed",width:"576px",height:"160px",textAlign:"center",top:"40px"}}>
-        <div style = {{width:"100%",height:"29px",fontFamily:"Noto Sans KR",fontSize:"20px",color:"#707070",lineHeight:"29px",marginTop:"40px",marginBottom:"10px"}}>
-          사용자 매뉴얼 디자인 등록01을 삭제했습니다.</div>
-        <div style = {{width:"100%",height:"29px",fontFamily:"Noto Sans KR",fontSize:"20px",textDecoration:"underline",color:"#FF0000"}}>
-          되돌리기</div>
-        <div onClick = {this.handleOnClickDeleteDesign} style={{position:"absolute",right:"-50px",top:"0px",width:"22px",height:"22px",
-                    backgroundImage: `url(${iDelete})`,backgroundSize: "cover", backgroundPosition: "center center",}}>
-        </div>
-      </Modal>
+    const DeleteCompleteModal = () => {
+      return (
+        <Modal open={this.state.deleteDialog} style={{ boxShadow: "0px 3px 6px #000000", position: "fixed", width: "576px", height: "160px", textAlign: "center", top: "40px" }}>
+          <div style={{ width: "100%", height: "29px", fontFamily: "Noto Sans KR", fontSize: "20px", color: "#707070", lineHeight: "29px", marginTop: "40px", marginBottom: "10px" }}>
+            사용자 매뉴얼 디자인 등록01을 삭제했습니다.</div>
+          <div style={{ width: "100%", height: "29px", fontFamily: "Noto Sans KR", fontSize: "20px", textDecoration: "underline", color: "#FF0000" }}>
+            되돌리기</div>
+          <div onClick={this.handleOnClickDeleteDesign} style={{
+            position: "absolute", right: "-50px", top: "0px", width: "22px", height: "22px",
+            backgroundImage: `url(${iDelete})`, backgroundSize: "cover", backgroundPosition: "center center",
+          }}>
+          </div>
+        </Modal>
       );
     }
     return (<>
 
-      <DeleteDesignModal/>
+      <DeleteDesignModal />
 
       <div style={modify_Main_Banner}>
         <div style={modify_Main_Banner_text}>디자인 수정하기</div>
@@ -152,27 +143,26 @@ class ModifyDesign extends Component {
 
       <div style={modify_Main_Section}>
         {/* scroll - menu */}
-        <div style={{width:"433px"}}>
-              <div style={{ width: "325px", marginLeft: "64px" }}>
-                  <div style={{ position: "fixed", top: "197px", width: "325px", height: "190px", backgroundColor: "#F5F4F4", borderRadius: "5px" }}>
-                    {scrollmenu.map((menu, index) => {
-                      return (<div onClick={() => this.gotoStep(menu, index)} style={{ cursor: "pointer", height: "62px", lineHeight: "29px", borderBottom: index + 1 === scrollmenu.length ? "none" : "2px solid #FFFFFF", paddingTop: "18px", paddingLeft: "36px" }} key={menu.txt}>
-                        <div style={{ color: this.state.step === index ? "#FF0000" : "#707070", fontSize: "20px", fontFamily: "Noto Sans KR", fontWeight: "300", textAlign: "left" }}>{menu.txt}</div>
-                      </div>)
-                    })}
-                  </div>
-              </div>
-              <div onClick = {this.handleOnClickDeleteDesign} style={modify_Menu_Delete}>디자인 삭제하기</div>
+        <div style={{ width: "433px" }}>
+          <div style={{ width: "325px", marginLeft: "64px" }}>
+            <div style={{ position: "fixed", top: "197px", width: "325px", height: "190px", backgroundColor: "#F5F4F4", borderRadius: "5px" }}>
+              {scrollmenu.map((menu, index) => {
+                return (<div onClick={() => this.gotoStep(menu, index)} style={{ cursor: "pointer", height: "62px", lineHeight: "29px", borderBottom: index + 1 === scrollmenu.length ? "none" : "2px solid #FFFFFF", paddingTop: "18px", paddingLeft: "36px" }} key={menu.txt}>
+                  <div style={{ color: this.state.step === index ? "#FF0000" : "#707070", fontSize: "20px", fontFamily: "Noto Sans KR", fontWeight: "300", textAlign: "left" }}>{menu.txt}</div>
+                </div>)
+              })}
+            </div>
+          </div>
+          <div onClick={this.handleOnClickDeleteDesign} style={modify_Menu_Delete}>디자인 삭제하기</div>
         </div>
         {/* form */}
         <div style={{ width: "1422px", height: "871px", borderRadius: "5px", border: "8px solid #F5F4F4", paddingTop: "46px" }}>
-          <form>
-             
-             {step ===0 && <BasicInfo 
-             onChangeExplain={this.handleInputDesignExplain} onChangeTitle = {this.handleInputDesignTitle} onChangeThumbnail = {this.handleChangeThumbnail}
-             designExplain={this.state.designExplain} designTitle ={this.state.designTitle} designThumbnail = {this.state.thumbnail} {...this.props}/> }
-             {step ===1 &&<AdditionalInfo {...this.props}/>}
-             {step ===2 &&<DesignEditor {...this.props}/>}
+          <form onKeyPress={this.preventEnterKey}>
+            {step === 0 && <BasicInfo
+              onChangeExplain={this.handleInputDesignExplain} onChangeTitle={this.handleInputDesignTitle} onChangeThumbnail={this.handleChangeThumbnail}
+              designExplain={this.state.designExplain} designTitle={this.state.designTitle} designThumbnail={this.state.thumbnail} {...this.props} />}
+            {step === 1 && <AdditionalInfo {...this.props} />}
+            {step === 2 && <DesignEditor {...this.props} />}
             {/* buttons*/}
             <div style={{ marginTop: "20.54px", justifyContent: "flex-end", display: "flex" }}>
               {step === 0
