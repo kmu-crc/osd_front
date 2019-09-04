50734e99c03c7225b36069d6c7efee71c2f0cde7
ggamsang <w.ggmsng@gmail.com>
2019-09-04 16:52:20 +0900

    message alarm

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -42,12 +42,11 @@ const DesignCreateBtn = styled.div`
     cursor: pointer;
 `
 class Header extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
-        this.state = {noti:{},selectCate:-1};
+        this.state = { noti: {}, selectCate: -1 };
         this.gotoCreateDesignPage = this.gotoCreateDesignPage.bind(this);
-        
+
     }
 
     componentDidMount() {
@@ -73,23 +72,23 @@ class Header extends Component {
             <Menu className={(this.context.hidemenu ? " hidemenu" : "")}>
                 <ul className={"menu"} style={{ minWidth: "523px", margin: "0px", padding: "0px", listStyle: "none", display: "flex", lineHeight: "29px", verticalAlign: "top" }} >
                     <li style={{ minWidth: "55px", height: "55px", marginLeft: "10px" }}>
-                        <a  href="/"><img alt="logo" style={{ width: "55px", height: "55px" }} src={logo} /></a></li>
+                        <a href="/"><img alt="logo" style={{ width: "55px", height: "55px" }} src={logo} /></a></li>
                     <li style={{ minWidth: "55px", height: "29px", marginLeft: "50px", marginTop: "11px" }}>
-                        <a  style={(window.location.pathname === '/design') ? { color: "red" } : { color: "#707070" }} href="/design">디자인</a></li>
+                        <a style={(window.location.pathname === '/design') ? { color: "red" } : { color: "#707070" }} href="/design">디자인</a></li>
                     <li style={{ minWidth: "37px", height: "29px", marginLeft: "50px", marginTop: "11px" }}>
-                        <a  style={(window.location.pathname === '/group') ? { color: "red" } : { color: "#707070" }} href="/group">그룹</a></li>
+                        <a style={(window.location.pathname === '/group') ? { color: "red" } : { color: "#707070" }} href="/group">그룹</a></li>
                     <li style={{ minWidth: "74px", height: "29px", marginLeft: "50px", marginTop: "11px" }}>
                         <a style={(window.location.pathname === '/designer') ? { color: "red" } : { color: "#707070" }} href="/designer">디자이너</a></li>
                 </ul>
                 <ul style={{ margin: "0px", padding: "0px", listStyle: "none", display: "flex", lineHeight: "29px", verticalAlign: "top" }} >
                     <li style={{ minWidth: "327px", height: "36px", marginRight: "50px", marginTop: "9px", border: "none" }}>
-                        <SearchForm searchCategory = {this.state.selectCate} visible={window.location.href.search('/search') > -1 ? 0 : 1} /></li>
+                        <SearchForm searchCategory={this.state.selectCate} visible={window.location.href.search('/search') > -1 ? 0 : 1} /></li>
                     <li style={{ width: "34px", height: "34px", marginRight: "50px", marginTop: "10px" }}>
-                        {this.props.userInfo!=null&&<Message />}
-                        </li>
+                        {this.props.userInfo != null && <Message noti={this.state.noti} />}
+                    </li>
                     <li style={{ width: "34px", height: "34px", marginRight: "50px", marginTop: "10px" }}>
-                        {this.props.userInfo!=null&&<AlarmContainer {...this.props} />}
-                        </li>
+                        {this.props.userInfo != null && <AlarmContainer {...this.props} />}
+                    </li>
                     <li style={{ minWidth: "97px", lineHeight: "29px", height: "29px", marginRight: "50px", marginTop: "11px" }}>
                         <DesignCreateBtn onClick={this.gotoCreateDesignPage}>디자인 등록</DesignCreateBtn></li>
                     <li style={{ minWidth: "55px", height: "29px", marginRight: "17px", marginTop: "11px" }}>
