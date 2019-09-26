e5fc364442e2f7597d68492cd81345c45950cc68
ggamsang <w.ggmsng@gmail.com>
2019-09-26 20:59:15 +0900

    editor file added

---
+++ src/components/Designs/CardSourceDetail/TextControllerClassic.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+class TextControllerClassic extends Component {
+    render() {
+        const { item } = this.props;
+        return (
+            <div className="App">
+                <p>(editor: CKEditor 5)</p>
+                <CKEditor
+                    editor={ClassicEditor}
+                    data={item.content}//"<p>Hello from CKEditor 5!</p>"
+                    onInit={editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log('Editor is ready to use!', editor);
+                    }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        this.props.getValue({content:data});
+                        console.log({ event, editor, data });
+                    }}
+                    onBlur={(event, editor) => {
+                        console.log('Blur.', editor);
+                    }}
+                    onFocus={(event, editor) => {
+                        console.log('Focus.', editor);
+                    }}
+                />
+            </div>
+        );
+    }
+}
+
+export default TextControllerClassic;
