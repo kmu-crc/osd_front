3df7b531d1935bb0a97307cc95aebd285bc460aa
ggamsang <w.ggmsng@gmail.com>
2020-01-08 17:49:39 +0900

    proto3rd-commit: create-maker-page

--- src/components/Maker/CreateMaker/CreateMaker.js
+++ src/components/Maker/CreateMaker/CreateMaker.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
-import { FormInput,FormAddress,FormExp,FormTag ,FormThumbnail, FormDropBox,FormCheckBox_new } from "components/Commons/FormItems";
-
+import { FormInput, FormAddress, FormExp, FormTag, FormThumbnail, FormDropBox, FormCheckBox_new } from "components/Commons/FormItems";
+import Button from "components/Commons/Button";
 import { Header, Grid } from "semantic-ui-react"
 import StyleGuide from "StyleGuide";
 
 
 const category = [
-  {text:"특허권",value:0},
-  {text:"디자인권",value:1},
-  {text:"기술자문",value:2},
-  {text:"기술상담",value:3},
-  {text:"경험",value:4},
-  {text:"정보/데이터",value:5},
-  {text:"아이디어/노하우",value:6},
-  {text:"제품",value:7},
+  { text: "특허권", value: 0 },
+  { text: "디자인권", value: 1 },
+  { text: "기술자문", value: 2 },
+  { text: "기술상담", value: 3 },
+  { text: "경험", value: 4 },
+  { text: "정보/데이터", value: 5 },
+  { text: "아이디어/노하우", value: 6 },
+  { text: "제품", value: 7 },
 ];
 
 const FromFieldCard = styled.div`
@@ -35,7 +35,7 @@ const FromFieldCard = styled.div`
   }
 `;
 
-const FormHeader = styled(Header) `
+const FormHeader = styled(Header)`
   position: relative;
   padding-right: 2.5rem !important;
   @media only screen and (max-width: 991px) {
@@ -77,22 +77,22 @@ const Label = styled.div`
 
 class CreateMaker extends Component {
   render() {
-    return(
-       <React.Fragment>
-         <div>
+    return (
+      <React.Fragment>
+        <div>
           <form onSubmit={this.onSubmit}>
-          <FromFieldCard>
+            <FromFieldCard>
               <Grid>
                 <Grid.Column mobile={16} computer={4}>
                   <FormHeader as="h2">메이커 정보</FormHeader>
                 </Grid.Column>
                 <Grid.Column mobile={16} computer={12}>
-                <Label>썸네일 등록</Label>
+                  <Label>썸네일 등록</Label>
                   <FormThumbnail
                     name="thumbnail"
                     placeholder="썸네일 등록"
                     getValue={this.onChangeValue}
-                    onChange={()=>{this.liveCheck("thumbnail")}}
+                    onChange={() => { this.liveCheck("thumbnail") }}
                     validates={["Required", "OnlyImages", "MaxFileSize(10000000)"]}
                   />
                   <Label>카테고리</Label>
@@ -108,28 +108,30 @@ class CreateMaker extends Component {
                     getValue={this.onChangeValue}
                   />
                   <Label>위치</Label>
-                  <FormAddress/>
+                  <FormAddress />
                   <Label>경력</Label>
-                  <FormExp/>
+                  <FormExp />
                   <Label>태그</Label>
                   <FormTag
-                  placeholder="태그를 입력해주세요(한글10자 영문20자 이내)"/>
+                    placeholder="태그를 입력해주세요(한글10자 영문20자 이내)" />
                   <Label>보유장비</Label>
-                  <FormCheckBox_new 
-                  items="장비1장비1,장비2장비2,장비3장비3,장비4장비4,장비5장비5,장비6장비6,장비7장비7,장비8장비8,장비9장비9,장비10장비10"/>
-                  <div className="miniLabel">추가입력</div><FormTag/>
-                  
-                  <Label>보유기술</Label>
-                  <FormCheckBox_new 
-                  items="기술1기술1,기술2기술2,기술3기술3,기술4기술4,기술5기술5,기술6기술6,기술7기술7,기술8기술8,기술9기술9,기술10기술10"/>
-                  <div className="miniLabel">추가입력</div><FormTag/>
+                  <FormCheckBox_new
+                    items="장비1장비1,장비2장비2,장비3장비3,장비4장비4,장비5장비5,장비6장비6,장비7장비7,장비8장비8,장비9장비9,장비10장비10" />
+                  <div className="miniLabel">추가입력</div><FormTag />
 
+                  <Label>보유기술</Label>
+                  <FormCheckBox_new
+                    items="기술1기술1,기술2기술2,기술3기술3,기술4기술4,기술5기술5,기술6기술6,기술7기술7,기술8기술8,기술9기술9,기술10기술10" />
+                  <div className="miniLabel">추가입력</div><FormTag />
                 </Grid.Column>
               </Grid>
             </FromFieldCard>
-            </form>
-          </div>
-       </React.Fragment>
+            <div style={{ width: "max-content", marginLeft: "auto" }}>
+              <Button color="Primary">등록하기</Button>
+            </div>
+          </form>
+        </div>
+      </React.Fragment>
     );
   }
 }
--- src/pages/CreateMakerPage/CreateMakerPage.js
+++ src/pages/CreateMakerPage/CreateMakerPage.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import CreateMakerContainer from 'containers/Maker/CreateMakerContainer/CreateMakerContainer'
+
 class CreateMakerPage extends Component {
   render() {
-    return(
-       <React.Fragment>
-         <CreateMakerContainer/>
-       </React.Fragment>
-    );
+    return (<CreateMakerContainer />);
   }
 }
 
