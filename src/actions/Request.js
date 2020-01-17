54ca1351b308b9b570f0599b9f5d3cd96de74cf6
ggamsang <w.ggmsng@gmail.com>
2020-01-17 17:55:25 +0900

    request comment

--- src/actions/Request.js
+++ src/actions/Request.js
@@ -68,3 +68,61 @@ export const GetRequestDetailRequest = (id) => {
   }
 };
 const GetRequestDetail = (data) => ({ type: types.GET_REQUEST_DETAIL, Detail: data });
+
+
+//COMMENT
+
+// 카드 댓글 가져오기
+export const GetRequestCommentRequest = (id) => {
+  return dispatch => {
+    dispatch(GetRequestComment());
+    const url = `${host}/request/comment/${id}`;
+    return fetch(url, {
+      headers: { "Content-Type": "application/json" },
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(res => dispatch(GetRequestCommentSuccess(res.data)))
+      .catch(error => dispatch(GetRequestCommentFailure(error)));
+  };
+};
+const GetRequestComment = () => ({ type: types.GET_REQUEST_COMMENT });
+const GetRequestCommentSuccess = data => ({ type: types.GET_REQUEST_COMMENT_SUCCESS, Comment: data });
+const GetRequestCommentFailure = error => ({ type: types.GET_REQUEST_COMMENT_FAILURE });
+
+// 의뢰 댓글 생성
+export const CreateRequestCommentRequest = (data, id, token) => {
+  return dispatch => {
+    dispatch(CreateRequestComment());
+    const url = `${host}/request/comment-create/${id}`
+    return fetch(url, {
+      headers: { "x-access-token": token, "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(res => dispatch(CreateRequestCommentSuccess(res)))
+      .catch(error => dispatch(CreateRequestCommentFailure(error)));
+  };
+};
+const CreateRequestComment = () => ({ type: types.CREATE_REQUEST_COMMENT });
+const CreateRequestCommentSuccess = res => ({ type: types.CREATE_REQUEST_COMMENT_SUCCESS, data: res });
+const CreateRequestCommentFailure = error => ({ type: types.CREATE_REQUEST_COMMENT_FAILURE });
+
+// 의뢰 댓글 삭제
+export const DeleteRequestCommentRequest = (id, token) => {
+  return dispatch => {
+    dispatch(DeleteRequestComment());
+    const url = `${host}/request/comment-remove/${id}`
+    return fetch(url, {
+      headers: { "x-access-token": token, "Content-Type": "application/json" },
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(res => dispatch(DeleteRequestCommentSuccess(res)))
+      .catch(error => dispatch(DeleteRequestCommentFailure(error)));
+  };
+};
+const DeleteRequestComment = () => ({ type: types.DELETE_REQUEST_COMMENT });
+const DeleteRequestCommentSuccess = res => ({ type: types.DELETE_REQUEST_COMMENT_SUCCESS, data: res });
+const DeleteRequestCommentFailure = error => ({ type: types.DELETE_REQUEST_COMMENT_FAILURE });
