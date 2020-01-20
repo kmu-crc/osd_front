a9666222b03c5d5f82b5018172b6a93aa8062613
catchico <iana6528@gmail.com>
2020-01-20 17:48:14 +0900

    로그인 디자인

--- src/components/Registration/SignInForm/SignInForm.js
+++ src/components/Registration/SignInForm/SignInForm.js
@@ -5,39 +5,97 @@ import Button from "components/Commons/Button";
 import { FormInput } from "components/Commons/FormItems";
 import { FormControl, ValidationGroup } from "modules/FormControl";
 
-const BtnWrap = styled.div`
-  margin-bottom: 1rem;
-`;
-
-const LoginBtn = styled(Button)`
-  margin-bottom: 1rem;
-`;
-
-const Subinfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: start;
-  color: rgba(0, 0, 0, 0.87);
-  & .signUpBtn {
-    text-decoration: underline;
+const MainBox = styled.div`
+  *{
+    font-family:Noto Sans KR,Medium;
   }
-`;
 
-const FindPwBtn = styled.button`
-  cursor: pointer;
-  background-color: transparent;
-  border: 0;
-  margin-bottom: 1rem;
-`;
+  width:933px;
+  height:609px;
+  padding:28px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
 
-const Label = styled.div`
-  margin: 0 0 0.8rem 0;
-  display: block;
-  color: rgba(0, 0, 0, 0.87);
-  font-size: 0.92857143em;
-  font-weight: 700;
-  text-transform: none;
-`;
+  .contentsBox{
+    width:498px;
+    height:100%;
+    display:flex;
+    flex-direction:column;
+
+    .titleBox{
+      width:100%;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      .title{
+        font-size:20px;
+      }
+    }
+    .row{
+      *{
+        font-size:16px;
+      }
+      display:flex;
+      height:43px;
+      margin-bottom:19px;
+      .label{
+        min-width:104px;
+        font-weight:60px;
+        display:flex;
+        align-items:center;
+      }
+      .red_text{
+        color:red;
+      }
+    }
+    .spaceBetween{
+      justify-content:space-between;
+    }
+
+  }
+`
+const InputTextBox = styled.input.attrs({type:'text'})`
+  border:none;
+  width:100%;
+  height:100%;
+  background-color:#E9E9E9;
+  border-radius:21px;
+  display:flex;
+  justify-content:center;
+  outline:none;
+`
+const CustomButton = styled.div`
+  width:${props=>props.width}px;
+  height:${props=>props.height}px;
+  border:1px solid ${props=>props.borderColor};
+  border-radius:${props=>props.borderRadius}px;
+  background-color:${props=>props.bgColor};
+  color:${props=>props.fontColor};
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  opacity:1;
+  &:hover{
+    opacity:0.7;
+  }
+
+  `
+const CheckBox = styled.div`
+  width:17px;
+  height:17px;
+  border:0.5px solid #707070;
+  margin-right:11px;
+`
+const CustomBox = styled.div`
+  width:${props=>props.width==null?100:props.width}%;
+  height:${props=>props.height==null?100:props.height}%;
+  display:flex;
+  margin-top:${props=>props.marginTop == null?0:props.marginTop}px;
+  margin-bottom:${props=>props.marginBottom == null?0:props.marginBottom}px;
+  margin-left:${props=>props.marginLeft == null?0:props.marginLeft}px;
+  margin-right:${props=>props.marginRight == null?0:props.marginRight}px;
+`
 
 class SignInForm extends Component {
   onChangeValue = async data => {
@@ -89,63 +147,79 @@ class SignInForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
-        <Label>Email</Label>
-        <FormInput
-          name="email"
-          placeholder="email을 입력해주세요."
-          getValue={this.onChangeValue}
-          validates={["Required", "IsEmail"]}
-          onBlur={() => {
-            this.liveCheck("email");
-          }}
-        />
-        <Label>Password</Label>
-        <FormInput
-          name="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-          getValue={this.onChangeValue}
-          validates={["Required", "MinLength(2)", "NotBlank"]}
-          onBlur={() => {
-            this.liveCheck("password");
-          }}
-        />
-        <Subinfo>
-          <Link to="/resetPw">
-            <FindPwBtn type="button">비밀번호 찾기</FindPwBtn>
-          </Link>
-          <Link to="/signup" className="signUpBtn">
-            회원가입
-          </Link>
-        </Subinfo>
-        <BtnWrap>
-          <LoginBtn type="submit" round={true} fluid={true}>
-            로그인
-          </LoginBtn>
-{/*
-          <FacebookLogin
-            appId="799483036860094"
-            autoLoad={false}
-            callback={this.onClickFBSignInbtn}
-            render={renderProps => (
-              <Button
-                onClick={renderProps.onClick}
-                type="button"
-                color="facebook"
-                icon="facebook f"
-                round={true}
-                fluid={true}
-              >
-                FaceBook 로그인
-              </Button>
-            )}
-          />
-*/}
-        </BtnWrap>
-      </form>
+        <React.Fragment>
+          <form>
+            <MainBox>
+              <div className="contentsBox">
+                <div className="titleBox"><div className="title">로그인</div></div>
+                  <CustomBox height={14.8}/>
+                  <div className="row">
+                    <div className="label"><div>아이디</div></div>
+                    <InputTextBox/>
+                  </div> 
+                  <div className="row">
+                    <div className="label"><div>비밀번호</div></div>
+                    <InputTextBox/>
+                  </div>
+                  <div className="row">
+                    <div className="label"/>
+                    <CustomBox width={29} ><CheckBox/>아이디 저장</CustomBox>
+                    <CustomBox width={29}><CheckBox/>로그인 상태 유지</CustomBox>
+                  </div>
+                  <div className="row">
+                  <CustomButton width={498} height={43} 
+                  bgColor={"#FF0000"} borderRadius={21} fontColor={"white"}>로그인</CustomButton>
+                  </div>
+                  <div className="row spaceBetween">
+                    <CustomButton width={251} height={43} 
+                    bgColor={"white"} borderRadius={21} borderColor={"red"} fontColor={"red"}>회원가입</CustomButton>
+                    <div className="label red_text ">아이디/비밀번호 찾기</div>
+                  </div>
+                </div>
+
+            </MainBox>
+          </form>
+        </React.Fragment>
     );
   }
 }
 
 export default SignInForm;
+
+
+      // <form onSubmit={this.onSubmit}>
+      //   <Label>Email</Label>
+      //   <FormInput
+      //     name="email"
+      //     placeholder="email을 입력해주세요."
+      //     getValue={this.onChangeValue}
+      //     validates={["Required", "IsEmail"]}
+      //     onBlur={() => {
+      //       this.liveCheck("email");
+      //     }}
+      //   />
+      //   <Label>Password</Label>
+      //   <FormInput
+      //     name="password"
+      //     type="password"
+      //     placeholder="비밀번호를 입력해주세요."
+      //     getValue={this.onChangeValue}
+      //     validates={["Required", "MinLength(2)", "NotBlank"]}
+      //     onBlur={() => {
+      //       this.liveCheck("password");
+      //     }}
+      //   />
+      //   <Subinfo>
+      //     <Link to="/resetPw">
+      //       <FindPwBtn type="button">비밀번호 찾기</FindPwBtn>
+      //     </Link>
+      //     <Link to="/signup" className="signUpBtn">
+      //       회원가입
+      //     </Link>
+      //   </Subinfo>
+      //   <BtnWrap>
+      //     <LoginBtn type="submit" round={true} fluid={true}>
+      //       로그인
+      //     </LoginBtn>
+      //   </BtnWrap>
+      // </form>
