fd540ca7b6686ff6142c9ab054651a326bce970b
ggamsang <w.ggmsng@gmail.com>
2020-02-05 09:48:47 +0900

    message-list re-design

--- src/pages/MessagePage/MessagePage.js
+++ src/pages/MessagePage/MessagePage.js
@@ -3,10 +3,11 @@ import MessageListContainer from "containers/Messages/MessageListContainer";
 
 class MessagePage extends Component {
   render() {
-    return(
-        <MessageListContainer history={this.props.history}
-                              id={this.props.match.params.id? this.props.match.params.id : null}
-                              name={this.props.match.params.name? this.props.match.params.name : null}/>
+    return (
+      <MessageListContainer
+        history={this.props.history}
+        id={this.props.match.params.id ? this.props.match.params.id : null}
+        name={this.props.match.params.name ? this.props.match.params.name : null} />
     );
   }
 }
