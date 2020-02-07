3f11254a13c79219567b4146c582d5771a5132a4
ggamsang <w.ggmsng@gmail.com>
2020-02-07 10:37:43 +0900

    item-detail

--- src/components/Items/ItemDetail/ItemDetail.js
+++ src/components/Items/ItemDetail/ItemDetail.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import 'react-dropdown/style.css';
 import Star from "components/Commons/Star";
-import DesignDetailStepContainer from "containers/Designs/DesignDetailStepContainer";
+import noimg from "source/noimg.png";
+
+// import DesignDetailStepContainer from "containers/Designs/DesignDetailStepContainer";
 import DesignDetailViewContainer from "containers/Designs/DesignDetailViewContainer";
-// import noimg from "source/noimg.png";
 // import who from "source/thumbnail.png";
 // import { Dropdown } from "semantic-ui-react";
 // import TextFormat from 'modules/TextFormat';
 // import cookie from 'react-cookies';
 
 const Wrapper = styled.div`
-  *{ border:1px solid red; };
+  // *{ border:1px solid red; };
   margin-top: 50px;
   .line { display: flex; }
 `;
@@ -106,7 +107,9 @@ const ItemInfo = styled.div`
       width: 213px;
       height: 70px;
       .text{
-        margin-left: 61px;
+        width: max-content;
+        margin-left: auto;
+        margin-right: auto;
         margin-top: 15px;
         line-height: 37px;
         font-size: 25px;
@@ -202,17 +205,6 @@ const Reviews = styled.div`
   opacity: 1;
   // padding: 
 `;
-const empty = {
-  title: "Lorem Ipsum", nickName: "fdnwodfowfdn", price: 18000, rate: 4, reviews: 30,
-  options: [{ type: "combo", text: "모양", data: ["세모", "네모", "동그라미"] }],
-  amount: 3000, who: "https://s3.ap-northeast-2.amazonaws.com/osd.uploads.com/thumbnails/1574308943657-x200.PNG",
-  thumbnail: "",
-  mainImage: "https://s3.ap-northeast-2.amazonaws.com/osd.uploads.com/thumbnails/1550043018657-x200.jpg",
-  subImages: ["", "", "", "", ""],
-
-  // variety section
-  detail: "천지는 맺어, 끓는 밥을 곧 것이다. 영원히 고동을 불러 심장은 피가 봄바람을 인생에 있으랴? 불어 커다란 할지라도 부패를 인간의 생명을 이상, 불어 바로 것이다. 대고, 방황하였으며, 가치를 봄날의 인간이 가진 설산에서 운다. 있는 착목한는 그들의 노래하며 원질이 대한 아름다우냐? 같은 찬미를 붙잡아 청춘 힘차게 두기 갑 속잎나고, 소담스러운 것이다. 몸이 원질이 가슴이 피가 반짝이는 소리다.이것은 이상의 예가 피다. 그들을 할지니, 품었기 가치를 보배를 남는 지혜는 약동하다. 목숨이 일월과 동력은 가는 청춘의 사라지지 더운지라 가는 있음으로써 것이다. 가치를 웅대한 대한 새 피가 품에 소담스러운 그들에게 오직 듣는다. 찾아다녀도, 들어 그들은 피어나기 것이다. 착목한는 되려니와, 그와 타오르고 커다란 가는 위하여서. 물방아 얼마나 것이다.보라, 바로 얼마나 남는 위하여서, 봄바람이다. 얼마나 그림자는 얼음에 보이는 새가 보내는 것이다. 가슴에 인간의 두기 끝까지 무엇이 것은 그리하였는가? 보이는 천지는 주며, 듣는다. 이상, 몸이 곧 두기 커다란 이것을 그들에게 위하여서, 가슴에 보라. 무한한 돋고, 많이 가슴에 있는 사막이다. 힘차게 무엇을 능히 되는 가치를 이 거선의 남는 부패뿐이다. 소금이라 얼음 긴지라 품었기 과실이 굳세게 끓는 봄바람이다. 인간의 갑 별과 사라지지 품에 같지 사막이다. 소금이라 듣기만 설레는 심장은 있으며, 것은 위하여서, 그리하였는가? 그들을 그러므로 물방아 우리의 있을 얼음과 청춘의 장식하는 보라. 이것은 끝까지 기관과 가진 인류의 그들은 힘있다. 붙잡아 뛰노는 실로 피고 피에 그것을 황금시대다. 그들의 위하여, 그것을 힘있다. 봄바람을 구하기 가슴이 풍부하게 주며, 무엇을 인도하겠다는 없으면, 봄바람이다. 청춘 방황하여도, 산야에 영원히 그들은 간에 하는 위하여서, 아니다. 사는가 얼마나 그들은 부패를 못할 하여도 무엇을 것이다. 찾아다녀도, 피는 위하여 약동하다."
-};
 const Page = styled.div`
     width: max-content;
     margin-top: 87px;
@@ -301,7 +293,7 @@ class ItemDetail extends Component {
     return !item ? (<div>loading...</div>) :
       (<Wrapper>
         <div className="line">
-          <ItemImages main={item.mainImage}>
+          <ItemImages main={item.img ? item.img[0].l_img : noimg}>
             <div className="main-image"></div>
             <div className="sub-images line">
               <div img={item.mainImage} className="sub nine-teen"></div>
@@ -316,16 +308,16 @@ class ItemDetail extends Component {
             <div className="title">{item.title}</div>
             <div className="expert line">
               <div className="who" />
-              <div className="nick">{item.nickName}</div>
+              <div className="nick">{item.userName}</div>
             </div>
 
             <div className="price-and-rate line">
-              <div className="price">{item.price}</div>
-              <div className="rate">{Star(item.rate)}({item.reviews})</div>
+              <div className="price" style={{ marginRight: "35px" }}>{item.price}</div>
+              <div className="rate line">{Star(item.rate)}({item.reviews || 0})</div>
             </div>
 
-            <div className="options">
-              {/* {item.options.map(opt => <Options key={opt} data={opt} />)} */}
+            {/* <div className="options">
+              {/ * {item.options.map(opt => <Options key={opt} data={opt} />)} * /}
               <div className="combo-wrapper line">
                 <div className="text">모양</div>
                 <div className="box WIDTH360"></div>
@@ -334,13 +326,13 @@ class ItemDetail extends Component {
                 <div className="text">수량</div>
                 <div className="box WIDTH178"></div>
               </div>
-            </div>
+            </div> */}
 
             <div className="buttons line">
               <div className="button first">
-                <div className="text">즉시구매</div></div>
+                <div className="text">아이템구매</div></div>
               <div className="button second">
-                <div className="text">장바구니</div></div>
+                <div className="text">관심항목추가</div></div>
             </div>
           </ItemInfo>
         </div>
@@ -353,14 +345,15 @@ class ItemDetail extends Component {
                 <div className="title">상품 상세설명</div>
                 <div className="text">{item.detail}</div>
               </Detail>
-              <Delivery mRight={102}>
-                <div className="title">배송정보</div>
+              <Delivery mRight={102} style={{ background: "#EFEFEF", fontSize: "36px", fontWeight: "500", padding: "35px" }}>
+                선택사항들어갈공간
+                {/* <div className="title">배송정보</div>
                 <div className="sub-title">제작기간</div>
                 <div className="text">3~5일</div>
                 <div className="sub-title">배송</div>
                 <div className="text">택배배송(무료)</div>
                 <div className="sub-title">반품</div>
-                <div className="text">반품료는 5000원이며 반품시 택배 박스와 함께 현금을 동봉해주시기 바랍니다.</div>
+                <div className="text">반품료는 5000원이며 반품시 택배 박스와 함께 현금을 동봉해주시기 바랍니다.</div> */}
               </Delivery>
             </div>
             <div style={{ marginTop: "95px" }}>
@@ -465,9 +458,12 @@ class ItemDetail extends Component {
 
         {/* item-detail */}
         <div className="line">
-          {item && item.is_project === 1 ? "project-view" : "blog-view"}
+          {/* {item && item.is_project === 1 ? "project-view" : "blog-view"} */}
+          <div style={{ padding: "45px", fontSize: "36px", fontFamily: "Noto Sans KR", fontWeight: "500", marginTop: "35px", borderRadius: "35px", width: "100%", background: "#EFEFEF", height: "350px" }}>
+            아이템 내용 들어갈 공간
+          </div>
           {/* ? <DesignDetailStepContainer design={item} {...this.state} /> */}
-          {/* : <DesignDetailViewContainer id={this.props.id} {...this.state} history={this.props.history} />} */}
+          {/* <DesignDetailViewContainer id={this.props.id} {...this.state} history={this.props.history} />} */}
         </div>
       </Wrapper>)
   }
