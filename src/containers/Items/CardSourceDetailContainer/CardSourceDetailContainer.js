0ec7dd7d3590c6cf80cbd7192caa4623e8b5caa9
ggamsang <w.ggmsng@gmail.com>
2020-03-11 10:15:29 +0900

    files moved, container file

---
+++ src/containers/Items/CardSourceDetailContainer/CardSourceDetailContainer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+// import {
+//   // GetDesignSourceRequest, 
+//   UpdateDesignSourceRequest, DesignSourceResetRequest
+// } from "actions/Designs/DesignCard";
+import { GetItemContentsRequest, UpdateItemContentsRequest } from "actions/Item";
+import CardSourceDetail from "components/Items/ItemDetail/CardSourceDetail";
+// import { UpdateDesignTime } from "actions/Designs/UpdateDesign"
+
+class CardSourceDetailContainer extends Component {
+  componentDidMount() {
+    this.props.cardId && this.props.GetItemContentsRequest(this.props.cardId, this.props.token);
+  }
+  render() {
+    return (
+      <CardSourceDetail {...this.props} upDateRequest={this.props.UpdateItemContentsRequest} />
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    token: state.Authentication.status.token,
+    content: state.ItemContent.status.content,
+    ItemDetail: state.ItemDetail.status.ItemDetail,
+    status: state.ItemContent.ItemContentEdit.status,
+    editStatus: state.ItemContent.ItemContentEdit.status
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  GetItemContentsRequest: (id) => dispatch(GetItemContentsRequest(id)),
+  UpdateItemContentsRequest: (data, card_id, token) => dispatch(UpdateItemContentsRequest(data, card_id, token)),
+
+  // DesignSourceResetRequest: () => { return dispatch(DesignSourceResetRequest()); },
+  // UpdateDesignTime: (design_id, token) => { return dispatch(UpdateDesignTime(design_id, token)) }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardSourceDetailContainer)
