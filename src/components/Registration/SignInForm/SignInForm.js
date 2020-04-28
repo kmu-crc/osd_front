8d4842c7ab1af727e439fcca678ece62c3fdb095
catchico <iana6528@gmail.com>
2020-04-28 17:05:26 +0900

    로그인 헤더

--- src/components/Registration/SignInForm/SignInForm.js
+++ src/components/Registration/SignInForm/SignInForm.js
@@ -12,20 +12,30 @@ class SignInForm extends Component {
   signin = () => {
     this.closeModal()
     this.setState({ signin_modal: false })
-    window.history.go(-1).then(
-      () => {
         if (window.location.href.search("SignUp") > -1) {
-          window.history.go(-1).then(window.location.reload())
+          alert("?")
+          window.history.go(-1)
+          .then(window.location.reload())
         }
         else {
-          window.location.reload()
+          window.history.go(-1)
         }
+    // window.history.go(-1).then(
+    //   () => {
+    //     if (window.location.href.search("SignUp") > -1) {
+    //       window.history.go(-1)
+    //       .then(window.location.reload())
+    //     }
+    //     else {
+    //       window.location.reload()
 
-      }
+    //     }
 
-    );
-    //console.log("history",window.history);return;
-    //window.location.reload()
+    //   }
+
+    // );
+    // //console.log("history",window.history);return;
+    // //window.location.reload()
   }
   closeModal = () => { this.setState({ signin_modal: false }) }
   signout = () => {
@@ -40,7 +50,8 @@ class SignInForm extends Component {
   }
   render() {
     return (
-      <SignInModal open={this.state.signin_modal} CheckEmailRequest={this.props.CheckEmailRequest} FindPwRequest={this.props.FindPwRequest} signinrequest={this.props.SignInRequest} signin={this.signin} close={this.closeModal} />
+      <SignInModal open={this.state.signin_modal} CheckEmailRequest={this.props.CheckEmailRequest} 
+      FindPwRequest={this.props.FindPwRequest} signinrequest={this.props.SignInRequest} signin={this.signin} close={this.closeModal} />
       // <form onSubmit={this.onSubmit}>
       // </form>
     );
