0ea8709d9cf28c6dce70fd2cd079773bbaf14df8
catchico <iana6528@gmail.com>
2020-05-15 10:48:04 +0900

    공지사항 게시판수정

--- src/Admin/components/DesignerManager/DesignerManager.js
+++ src/Admin/components/DesignerManager/DesignerManager.js
@@ -3,7 +3,138 @@ import host from "config";
 import Loading from "components/Commons/Loading";
 import DatePicker from 'react-date-picker';
 import { Dropdown } from "semantic-ui-react";
-import noimg from "source/thumbnail.png";
+import noimg from "source/noimg.png";
+import styled from "styled-components"
+import { Pagination } from 'semantic-ui-react'
+
+function ListElement({ item: { nick_name, email, imgURL, uid },item, handleTop,handleDel }) {
+  // console.log("title",title);
+  return <div style={{
+      position: "relative",
+      backgroundSize: "cover",
+      backgroundImage: `url(${imgURL==null?noimg:imgURL.m_img})`,
+      width: "150px",
+      height: "150px",
+      borderRadius: "5px",
+      marginRight: "5px",
+      marginBottom: "5px",
+  }}>
+      <div style={{width:"100%",display:"flex",justifyContent:"space-between"}}>
+          {/* <div
+          onClick={() => handleTop(uid)}
+          style={{
+              cursor: "pointer",
+              padding: "5px 10px",
+              width: "max-content",
+              color: "white",
+              backgroundColor: "orange",
+              borderRadius: "15px",
+          }}>인기디자인</div> */}
+      <div
+          onClick={() => handleDel(item)}
+          style={{
+              cursor: "pointer",
+              padding: "5px 10px",
+              marginLeft: "auto",
+              width: "max-content",
+              color: "white",
+              backgroundColor: "red",
+              borderRadius: "15px",
+          }}>삭제</div>
+        </div>
+      <div style={{
+          bottom: "0px",
+          width: "100%",
+          position: "absolute",
+          padding:"5px",
+          backgroundColor: "#707070",
+      }}>
+          <div title={email} style={{
+              padding: "1px 2px",
+              fontSize: "16px",
+              height: "20px",
+              width: "100%",
+              color: "white",
+              wordWrap: "break-word",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis"
+          }}>
+              {email}({uid})</div>
+          <div title={nick_name} style={{
+              padding: "1px 2px",
+              fontSize: "12px",
+              height: "16px",
+              width: "100%",
+              color: "white",
+              wordWrap: "break-word",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+              textAlign: "right"
+          }}>
+              {nick_name}</div>
+      </div>
+  </div>
+}
+const MainBox = styled.div`
+// *{
+//   border:1px solid black;
+// }
+  display:flex;
+  width:max-content;
+  flex-direction:row;
+  margin-left:auto;
+  margin-right:auto;
+  .main{
+    margin-top:20px;
+    margin-bottom:10px;
+  }
+  .pageRange{
+    width:100%;
+    margin-left:auto;
+    margin-right:auto;
+    margin-top:30px;
+    display:flex;
+    justify-content:center;
+    
+  }
+`
+const FilterBox = styled.div`
+  display:flex;
+  align-items:center;
+  .s_padding{
+    padding:10px;
+  }
+  .s_margin{
+    margin:10px;
+  }
+  .textRgn{
+    height:29px;
+    width:300px;
+  }
+  .btn{
+    width:100px;
+    height:29px;
+    display:flex;
+    align-item:center;
+    justify-content:center;
+    padding:5px;
+  }
+`
+
+const ListBox = styled.div`
+// border:1px solid black;
+  width:780px;
+  // height:max-content,
+  display:flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+  justify-content:center;
+  margin-top:30px;
+
+`
+
 
 function getFormatDate(date) {
   if (date == null) return;
@@ -24,7 +155,7 @@ class DesignerManager extends Component {
       loading: false,
 
       // list
-      page: 0, max: 10,
+      page: 0, max: 20,
       normal: [],
       count: null,
 
@@ -61,7 +192,7 @@ class DesignerManager extends Component {
     this.GetDesignerListCountRequest();
     this.setState({ loading: false });
   };
-  GetDesignerListRequest(page = 0, max = 10, cate1 = "0", cate2 = "0", sort = "update", desc = "desc", start = "2000-01-01", end = "2020-12-31", keyword = null) {
+  GetDesignerListRequest(page = 0, max = this.state.max, cate1 = "0", cate2 = "0", sort = "update", desc = "desc", start = "2000-01-01", end = "2020-12-31", keyword = null) {
     return new Promise((resolve, reject) => {
       const url = `${host}/admins/DesignerList/${page}/${max}/${cate1}/${cate2}/${sort}/${desc}/${start}/${end}/${keyword}`;
       console.log(url);
@@ -71,7 +202,7 @@ class DesignerManager extends Component {
         .catch(error => alert(error));
     });
   };
-  GetDesignerListCountRequest(page = 0, max = 10, cate1 = "0", cate2 = "0", sort = "update", desc = "desc", start = "2000-01-01", end = "2020-12-31", keyword = null) {
+  GetDesignerListCountRequest(page = 0, max = this.state.max, cate1 = "0", cate2 = "0", sort = "update", desc = "desc", start = "2000-01-01", end = "2020-12-31", keyword = null) {
     return new Promise((resolve, reject) => {
       const url = `${host}/admins/DesignerListCount/${page}/${max}/${cate1}/${cate2}/${sort}/${desc}/${start}/${end}/${keyword}`;
       console.log(url);
@@ -179,12 +310,16 @@ class DesignerManager extends Component {
     const { page, max, cate1, cate2, sort, desc, startDate, endDate, keyword } = this.state;
     this.GetDesignerListRequest(page, max, cate1, cate2, sort, desc ? "desc" : "asc", getFormatDate(startDate), getFormatDate(endDate), keyword ? keyword : "");
   }
-
+  async goPage(activePage) {
+    await this.setState({ page:activePage});
+    const { page, max, cate1, cate2, sort, desc, startDate, endDate, keyword } = this.state;
+    this.GetDesignerListRequest(activePage, max, cate1, cate2, sort, desc ? "desc" : "asc", getFormatDate(startDate), getFormatDate(endDate), keyword ? keyword : "");
+   }
   render() {
     console.log(this.props, this.state);
 
-    const { normal, count, page, loading, category1, cate1, category2, cate2, keyword, desc, sort } = this.state;
-    const lastPage = parseInt(count / 10, 10);
+    const { max,normal, count, page, loading, category1, cate1, category2, cate2, keyword, desc, sort } = this.state;
+    const lastPage = parseInt(count / max, 10);
 
     // category1
     const combocate1 =
@@ -213,100 +348,88 @@ class DesignerManager extends Component {
 
     console.log(this.state.count);
     return (
-      <div style={{ display: "flex", flexDirection: "row", marginLeft: "auto", marginRight: "auto", width: "max-content" }}>
+      <MainBox>
 
         {/* loading */}
         {loading ? <Loading /> : null}
 
         {/* designer manager */}
-        <div>
+        <div className="main">
           {/* title */}
           <h1>디자이너</h1>
 
-          <div style={{ width: "max-content", marginLeft: "auto", marginRight: "auto" }}>
             {/* filter */}
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <div style={{ padding: "10px 5px", }}>
-                <Dropdown
-                  defaultValue={cate1}
-                  options={combocate1}
-                  onChange={this.onChangeMainCate}
-                />
-              </div>
-              <div style={{ padding: "10px 5px", }}>
-                <Dropdown
-                  defaultValue={cate2}
-                  options={combocate2}
-                  onChange={this.onChangeSubCate}
-                />
-              </div>
-
-              <div style={{ padding: "10px 5px", }}>
-                <input onChange={e => this.setState({ keyword: e.target.value })} value={keyword} />keyword</div>
-              <div style={{ padding: "10px 5px", }}>
-                <Dropdown
+            
+            <div>
+              <FilterBox>
+              <Dropdown
+                  compact
+                  selection
                   defaultValue={sort}
                   options={combosort}
                   onChange={this.onChangeSort}
                 />
-              </div>
               <div
                 onClick={() => this.setState({ desc: !desc })}
-                style={{ padding: "10px 5px", }}>
+                className="s_padding">
                 {desc
                   ? <div>내림차순 ▼</div>
                   : <div>오림차순 △</div>}
                 {/* ▲ ▽  */}
               </div>
-              <div style={{ padding: "10px 5px", }}>
                 <DatePicker name="start" onChange={this.handleStartDateChange} value={this.state.startDate} minDate={new Date('1900-01-01')} /> ~
-                </div>
-              <div style={{ padding: "10px 5px", }}>
                 <DatePicker name="start" onChange={this.handleEndDateChange} value={this.state.endDate} maxDate={new Date()} />
-              </div>
-              <div>
-                <button onClick={this.search}>검색</button>
-              </div>
-            </div>
-
+            </FilterBox>
+            <FilterBox>
+                <input placeholder="검색어"  className="textRgn" onChange={e => this.setState({ keyword: e.target.value })} value={keyword} />
+                <button className="btn" onClick={this.search}>검색</button>
+            </FilterBox>
             {/* list */}
-            <div>
+            <ListBox>
               {normal && normal.length > 0 ?
                 normal.map(item => {
-                  return <div key={item.uid} style={{ display: "flex", flexDirection: "row" }}>
-                    <div><div style={{ width: "75px", height: "75px", borderRadius: "50%", backgroundImage: `url(${(item.imgURL && item.imgURL.m_img) || noimg}`, backgroundSize: "cover" }} /></div>
-                    <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.categoryName || "전체"}</div>
-                    <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.nick_name}</div>
-                    <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.email}</div>
-                    <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.create_time}</div>
-                    <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.update_time}</div>
-                    <div style={{ padding: "0px 10px", margin: "5px 15px" }}>
-                      <div
-                        onClick={() => this.DeleteDesignerRequest(item)}
-                        style={{ backgroundColor: "red", color: "white", width: "max-content", textAlign: "center", cursor: "pointer", borderRadius: "15px", padding: "3px 20px" }}
-                      >삭제</div></div>
-                  </div>;
-                }) : <div>데이터가 없습니다.</div>}
-            </div>
+                  return (
+                    <ListElement item={item}
+                    handleDel={this.DeleteDesignerRequest}/>
+                  )
+                  // <div key={item.uid} style={{ display: "flex", flexDirection: "row" }}>
+                  //   <div><div style={{ width: "75px", height: "75px", borderRadius: "50%", backgroundImage: `url(${(item.imgURL && item.imgURL.m_img) || noimg}`, backgroundSize: "cover" }} /></div>
+                  //   <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.categoryName || "전체"}</div>
+                  //   <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.nick_name}</div>
+                  //   <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.email}</div>
+                  //   <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.create_time}</div>
+                  //   <div style={{ padding: "0px 10px", margin: "5px 15px" }}>{item.update_time}</div>
+                  //   <div style={{ padding: "0px 10px", margin: "5px 15px" }}>
+                  //     <div
+                  //       onClick={() => this.DeleteDesignerRequest(item)}
+                  //       style={{ backgroundColor: "red", color: "white", width: "max-content", textAlign: "center", cursor: "pointer", borderRadius: "15px", padding: "3px 20px" }}
+                  //     >삭제</div></div>
+                  // </div>;
+                }) 
+                : <div>데이터가 없습니다.</div>}
+            </ListBox>
           </div>
-
-          <div>
-
-            <div style={{ width: "max-content", marginLeft: "auto", marginRight: "auto", display: "flex", flexDirection: "row" }}>
-              <div style={{ width: "50px" }}>
-                {page > 0 ? <div onClick={this.goPrev}>prev</div> : null}
-              </div>
-              <div style={{ width: "50px" }}>
-                <div style={{ textAlign: "center", borderRadius: "15px" }}>{this.state.page + 1}</div>
-              </div>
-              <div style={{ width: "50px" }}>
-                {lastPage > page + 1 ? <div onClick={this.goNext}>next</div> : null}
-              </div>
+          <div className="pageRange">
+          <div> 
+          <Pagination
+                activePage={this.state.page+1}
+                boundaryRange={0}
+                defaultActivePage={1}
+                ellipsisItem={null}
+                firstItem={null}
+                lastItem={null}
+                siblingRange={1}
+                totalPages={lastPage}
+                onPageChange={(event,{activePage})=>{
+                  this.goPage(activePage-1);
+                }}
+              />
             </div>
+
 
           </div>
         </div>
-      </div>
+      </MainBox>
 
     )
   }
