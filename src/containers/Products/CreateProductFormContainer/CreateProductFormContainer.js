a6b49d28f827e481bb725395e8ccef6f83e52bfb
ggamsang <w.ggmsng@gmail.com>
2020-06-29 11:36:43 +0900

    market create-maker

--- src/containers/Products/CreateProductFormContainer/CreateProductFormContainer.js
+++ src/containers/Products/CreateProductFormContainer/CreateProductFormContainer.js
@@ -9,20 +9,20 @@ class CreateProductFormContainer extends Component {
   componentDidMount() {
     console.log("designer:", this.props, this.props.keep == null);
     if (this.props.keep == null) {
-      if (this.props.userInfo.isDesigner === 0 || this.props.userInfo.isMaker === 0) {
-        alert("디자이너가 아닙니다. 개인정보 페이지에 가셔서 디자이너로 등록하여주세요.");
-        this.props.history.push("/myPage");
-        ;
-      }
+      // if (this.props.userInfo.isDesigner === 0 || this.props.userInfo.isMaker === 0) {
+      //   alert("디자이너가 아닙니다. 개인정보 페이지에 가셔서 디자이너로 등록하여주세요.");
+      //   this.props.history.push("/myPage");
+      //   ;
+      // }
     }
   }
 
   render() {
     return (
       <React.Fragment>
-        {this.props.keep || this.props.userInfo.isDesigner === 1 || this.props.userInfo.isMaker === 1
+        {/* {this.props.keep// || this.props.userInfo.isDesigner === 1 || this.props.userInfo.isMaker === 1 */}
           ? <CreateProductForm {...this.props} />
-          : <p style={{ color: "#FFF" }}> 권한을 확인 중입니다.</p>}
+        {/* : <p style={{ color: "#FFF" }}> 권한을 확인 중입니다.</p>} */}
       </React.Fragment>
     )
   }
