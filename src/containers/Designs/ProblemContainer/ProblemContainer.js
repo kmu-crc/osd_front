ffdae9c7f7baf3120637d044c1e24c5c801a2c66
catchico <iana6528@gmail.com>
2021-02-18 15:00:53 +0900

    문제 필터링

--- src/containers/Designs/ProblemContainer/ProblemContainer.js
+++ src/containers/Designs/ProblemContainer/ProblemContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import { withRouter } from "react-router-dom"
 import ProblemController from "components/Designs/CardSourceDetail/ProblemController";
-import { getProblemListRequest, getProblemDetailRequest, UpdateAnswerRequest } from "redux/modules/design/card";
+import { getProblemListFilterRequest,getProblemCategoryRequest, getProblemListRequest, getProblemDetailRequest, UpdateAnswerRequest } from "redux/modules/design/card";
 import { Modal } from "semantic-ui-react"
 import styled from 'styled-components';
 import Cross from "components/Commons/Cross";
@@ -30,9 +30,16 @@ const ModalBox = styled(Modal)`
 class ProblemContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = { category:[]}
   }
   componentDidMount() {
     this.props.getProblemListRequest(1);
+    this.props.getProblemCategoryRequest().then(()=>{
+      this.setState({category:[]});
+      this.props.ProblemCategory.map((item,index)=>{
+        this.setState({category:this.state.category.concat({value:item.id,text:item.category_name})});
+      })
+    })
   }
   render() {
     // console.log(this.props)
@@ -42,12 +49,12 @@ class ProblemContainer extends Component {
           this.props.open == true || this.props.item.content == "" ?
             <ModalBox open={this.props.open}>
               <div className="closeBox"> <Cross onClick={() => this.props.openModal(false)} angle={45} color={"#707070"} weight={1} width={33} height={33} /></div>
-              <ProblemController {...this.props} onCloseModal={() => this.props.openModal(false)} getValue={data => this.props.getValue(data)} />
+              <ProblemController Category={this.state.category} {...this.props} onCloseModal={() => this.props.openModal(false)} getValue={data => this.props.getValue(data)} />
             </ModalBox>
             :
             <React.Fragment>
               {/* <div className="closeBox"> <Cross onClick={()=>this.props.close()} angle={45} color={"#707070"} weight={1} width={33} height={33} /></div> */}
-              <ProblemController {...this.props} onCloseModal={() => this.props.openModal(false)} getValue={data => this.props.getValue(data)} />
+              <ProblemController Category={this.state.category} {...this.props} onCloseModal={() => this.props.openModal(false)} getValue={data => this.props.getValue(data)} />
             </React.Fragment>
         }
       </React.Fragment>
@@ -61,6 +68,7 @@ const mapStateToProps = (state) => {
     ProblemList: state.DesignCard.status.ProblemList,
     ProblemCount: state.DesignCard.status.ProblemCount,
     ProblemDetail: state.DesignCard.status.ProblemDetail,
+    ProblemCategory: state.DesignCard.status.Category,
     token: state.Authentication.status.token,
   };
 };
@@ -70,12 +78,18 @@ const mapDispatchToProps = (dispatch) => {
     getProblemListRequest: (page) => {
       return dispatch(getProblemListRequest(page));
     },
+    getProblemListFilterRequest: (category_id) => {
+      return dispatch(getProblemListFilterRequest(category_id));
+    },
     getProblemDetailRequest: (uid) => {
       return dispatch(getProblemDetailRequest(uid));
     },
     UpdateAnswerRequest: (token, data) => {
       return dispatch(UpdateAnswerRequest(token, data));
-    }
+    },
+    getProblemCategoryRequest: () => {
+      return dispatch(getProblemCategoryRequest());
+    },
   };
 };
 
