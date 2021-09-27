5d7955a2751409a49220cb35022dd05d61ea6289
ggamsang <w.ggmsng@gmail.com>
2021-09-27 15:05:26 +0900

    main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -247,14 +247,14 @@ export default class Main extends Component {
         <Slider ref={slider => (this.slider = slider)} {...settings}>
           <Banner height={height} >
             <img src={new_banner_step1} />
-            <a href={"/designerdetail/5343"}>
+            <a href={"/designdetail/5343"}>
               <ButtonOnImage src={main_banner_1_button} {...{ width: 952 * width / 1920, height: 397 * height / 1080, bottom: 272, right: 130 }} />
             </a>
           </Banner>
 
           <Banner height={height} >
             <img src={new_banner_step2} />
-            <a href={"/designerdetail/5344"}>
+            <a href={"/designdetail/5344"}>
               <ButtonOnImage src={main_banner_2_button} {...{ width: 952 * width / 1920, height: 397 * height / 1080, bottom: 154, right: 85 }} />
             </a>
           </Banner>
